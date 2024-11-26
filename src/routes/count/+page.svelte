1dbb34feb0c73a4d443477deb193b62d09882948
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-11-26 18:06:44 -0500

    created html skeleton

--- src/routes/count/+page.svelte
+++ src/routes/count/+page.svelte
@@ -1,11 +1,17 @@
 <script lang="ts">
-
+//variables
+    let count = $state<number>(0)
 </script>
 
 <div>
-
+    <div></div>
+    <div>
+        <button></button>
+        <button></button>
+        <button></button>
+    </div>
 </div>
 
 <style>
-    
+
 </style>
