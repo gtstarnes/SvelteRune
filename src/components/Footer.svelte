32f1f96923bf769095c9ea72cd728b1bcfc8a858
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-11-26 18:42:30 -0500

    added styles

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,12 +1,15 @@
 <script lang="ts">
-
+    const year = new Date().getFullYear
 </script>
 
 
 <footer>
-
+    <div>Copyright© {year}</div>
 </footer>
 
 <style>
-    
+    footer {
+        position: fixed;
+        bottom: 0;
+    }
 </style>
