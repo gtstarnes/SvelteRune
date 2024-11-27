4258544465f6488ab3a868ee28e6826d7d75ba27
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-11-26 19:24:01 -0500

    created html skeleton

--- src/routes/todo/+page.svelte
+++ src/routes/todo/+page.svelte
@@ -7,6 +7,7 @@
         }
     ])
     let newTask = ''
+    let error = ''
 
 //FUNCTIONS
     const addTask = () => {
@@ -23,7 +24,22 @@
 </script>
 
 <div>
-
+    <div>
+        <input type="text" placeholder="add new task" />
+        <button>Add</button>
+        <button>Mass Del</button>
+    </div>
+    <div>error</div>
+    <div>
+        {#each tasks as task (task.task)}
+            <div>
+                <input type="checkbox" />
+                <div>{task.task}</div>
+                <button>Edit</button>
+                <button>Del</button>
+            </div>
+        {/each}
+    </div>
 </div>
 
 <style>
