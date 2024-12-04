1ec1be30fb49aaa944fd4f2c88db25ec9d28c047
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-12-03 23:59:56 -0500

    added minor styles

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -5,9 +5,15 @@
 
 
 <h2>ENHANCE YOUR CALM</h2>
-<div>This {$page.status} error occured:</div>
+<div>This <span>{$page.status}</span> error occured:</div>
 <p>{$page.error?.message}</p>
 
 <style>
-
+    span {
+        font-size: 2rem;
+        color:aqua
+    }
+    p {
+        font-size: 4rem;
+    }
 </style>
