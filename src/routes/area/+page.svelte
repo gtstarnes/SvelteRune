cb3a2ee0fbd7ed83c34d15e75db08f711e278992
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-12-05 20:29:59 -0500

    created html skeleton

--- src/routes/area/+page.svelte
+++ src/routes/area/+page.svelte
@@ -3,9 +3,13 @@
 </script>
 
 <div>
-
+    <h3>Enter Height and Width</h3>
+    <div>
+        <input type="number" placeholder="height" />
+        <input type="number" placeholder="width"/>
+    </div>
 </div>
 
 <style>
-    
+
 </style>
