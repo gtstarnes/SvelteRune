218cf8788555d20cba795fb080d81a92a4f774a0
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-12-07 00:13:04 -0500

    reset

--- src/routes/parent/+page.svelte
+++ src/routes/parent/+page.svelte
@@ -1,15 +1,9 @@
 <script lang="ts">
-	import Child from "./child.svelte";
 
-
-let count = $state(0)
 </script>
 
 <div>
-    <button onclick={()=>count++}>
-        count (parent): {count}
-    </button>
-    <Child {count} />
+
 </div>
 
 <style>
