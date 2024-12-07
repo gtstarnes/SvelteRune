e86da096acf9c198dcec34ca8e3245f1ccb09f31
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-12-07 00:12:49 -0500

    initial create

---
+++ src/routes/parent/parent.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import Child from "./child.svelte";
+
+
+let count = $state(0)
+</script>
+
+<div>
+    <button onclick={()=>count++}>
+        count (parent): {count}
+    </button>
+    <Child {count} />
+</div>
+
+<style>
+
+</style>
