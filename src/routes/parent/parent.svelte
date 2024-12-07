3cc4023540e6a0b28c8b71fb3452793010dc8ea1
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-12-07 00:28:25 -0500

    made count bindable

--- src/routes/parent/parent.svelte
+++ src/routes/parent/parent.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import Child from "./child.svelte";
 
-    let {count = $bindable(0), childCount = $bindable(0)} = $props();
+    let {count = $bindable(0)} = $props();
 </script>
 
 <div>
