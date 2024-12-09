d9bea8bc286c269049b6f0733af96b815c577c45
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-12-08 20:02:30 -0500

    bound parent

--- src/routes/parent/+page.svelte
+++ src/routes/parent/+page.svelte
@@ -1,17 +1,17 @@
 <script lang="ts">
 	import Parent from "./parent.svelte";
 
-
+    let count = $state(0)
 </script>
 
 <div>
     <div>
-        Parent:
+        Parent: {count}
     </div>
     <div>
         Child:
     </div>
-    <Parent />
+    <Parent bind:count={count} />
 </div>
 
 <style>
