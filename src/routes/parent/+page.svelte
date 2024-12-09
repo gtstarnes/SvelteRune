bf00ed8045767be2e47bce6a785a99f76f99d7e9
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-12-08 19:29:43 -0500

    reset

--- src/routes/parent/+page.svelte
+++ src/routes/parent/+page.svelte
@@ -1,18 +1,7 @@
 <script lang="ts">
-	import Parent from "./parent.svelte";
-    import { child } from "./state.svelte";
-    let {count = $bindable(0)} = $props()
+
 </script>
 
-<div>
-    <div>
-        Parent: {count}
-    </div>
-    <div>
-        Child: {child.count}
-    </div>
-    <Parent bind:count={count} />
-</div>
 
 <style>
 
