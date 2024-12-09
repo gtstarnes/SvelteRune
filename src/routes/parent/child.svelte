ccc2f62f1dc44670375539dda7c5dcd0fc56cb63
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-12-08 20:12:39 -0500

    created add function

--- src/routes/parent/child.svelte
+++ src/routes/parent/child.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
+	import { count } from "./state.svelte";
 
+    const add = () => {
+        count.child = count.child + 1
+    }
 </script>
 
-<button>
+<button onclick={add}>
     Child
 </button>
 
