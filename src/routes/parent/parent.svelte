1b84a0d27c492579000a5c4aa18b461f6e4f46e7
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-12-08 20:18:44 -0500

    added comments

--- src/routes/parent/parent.svelte
+++ src/routes/parent/parent.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
+//IMPORTS
 	import Child from "./child.svelte";
-import { count } from "./state.svelte";
+    import { count } from "./state.svelte";
 
-
+//FUNCTIONS
     const add = () => {
         count.parent = count.parent + 1;
         count.child = count.parent
