59b69669aabf93de8e15431ac637749dc4388ec6
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-12-08 20:04:02 -0500

    connect child to count

--- src/routes/parent/child.svelte
+++ src/routes/parent/child.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
+    import {child} from "./state.svelte"
     let {count = 0}: {count: number} = $props();
-
+    child.count = count;
     const add =() => {
         count++
     }
