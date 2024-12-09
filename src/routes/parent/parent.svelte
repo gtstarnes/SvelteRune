db7be4e7c1dfe17f4da7066e5128a0a367085e50
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-12-08 19:29:52 -0500

    reset

--- src/routes/parent/parent.svelte
+++ src/routes/parent/parent.svelte
@@ -1,21 +1,8 @@
 <script lang="ts">
-	import Child from "./child.svelte";
-    import {child} from "./state.svelte"
 
-    let {count = $bindable(0)} = $props();
-
-    const add = () => {
-        count++
-        child.count = count
-    }
 </script>
 
-<div>
-    <button onclick={add}>
-        parent
-    </button>
-    <Child {count} />
-</div>
+
 
 <style>
 
