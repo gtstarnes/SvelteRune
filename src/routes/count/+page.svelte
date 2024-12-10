2f66534a29952fe655021f9fd5837101bd51bb6f
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-12-09 22:23:29 -0500

    reset

--- src/routes/count/+page.svelte
+++ src/routes/count/+page.svelte
@@ -1,54 +1,14 @@
 <script lang="ts">
 //VARIABLES
-    let count = $state<number>(0)
+
 //FUNCTIONS
-const add = () => {
-    count++
-}
 
-const sub = () => {
-    count--
-}
-
-const reset = () => {
-    count = 0
-}
 </script>
 
-<div class="container">
-    <div class="count">{count}</div>
-    <div>
-        <button class="counters" onclick={add}>Increase</button>
-        <button onclick={reset}>Reset</button>
-        <button class="counters" onclick={sub}>Decrease</button>
-    </div>
+<div>
+
 </div>
 
 <style>
-    .container {
-        height: 90vh;
-        width: 100%;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-    }
-    .count {
-        font-size: 6rem;
-        margin-bottom: 1rem;
-    }
-    .counters {
-        background-color: orange;
-    }
-    button {
-        height: 3rem;
-        width: 6rem;
-        border: none;
-        cursor: pointer;
-        font-weight: bold;
-    }
-    button:active {
-        scale:90%;
-        background-color: darkorange;
-    }
+
 </style>
