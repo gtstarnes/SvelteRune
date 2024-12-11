1456b1e95d679a4e3d99c646bbcef58164d9107f
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-12-11 18:44:52 -0500

    initial create

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,28 +1,11 @@
 <script lang="ts">
-    interface Details {
-        username: string,
-        password: string
-    }
-    let input = $state<Details>({username: "", password: ""});
 
-
-    const handleSubmit = (e: Event) => {
-      e.preventDefault
-    }
 </script>
 
-<form onsubmit={handleSubmit}>
-    <div>
-        <label for="username">Username</label>
-        <input id="username" name="username" type="text" bind:value={input.username} />
-    </div>
-    <div>
-        <label for="password">Password</label>
-        <input id="password" name="password" type="password" bind:value={input.password} />
-    </div>
-    <button>Login</button>
-</form>
+<div>
+
+</div>
 
 <style>
-
+    
 </style>
