03224900425074c30eed08d875947c9b32061a36
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-12-11 18:40:43 -0500

    bound inputs

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -3,17 +3,22 @@
         username: string,
         password: string
     }
-    let newDetails = $state<Details>({username: "", password: ""});
+    let input = $state<Details>({username: "", password: ""});
+
+
+    const handleSubmit = (e: Event) => {
+      e.preventDefault
+    }
 </script>
 
-<form>
+<form onsubmit={handleSubmit}>
     <div>
         <label for="username">Username</label>
-        <input id="username" name="username" type="text" />
+        <input id="username" name="username" type="text" bind:value={input.username} />
     </div>
     <div>
         <label for="password">Password</label>
-        <input id="password" name="password" type="password" />
+        <input id="password" name="password" type="password" bind:value={input.password} />
     </div>
     <button>Login</button>
 </form>
