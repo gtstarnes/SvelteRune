e2a1ece97bc723714f4e96076c7c33015a0a57ec
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-12-12 20:55:28 -0500

    created html to display users

--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -1,12 +1,21 @@
 <script lang="ts">
 	import SignUp from "./signUp.svelte";
+	import { users } from "./users.svelte";
 
 </script>
 
 <div>
     <SignUp />
     <div>
-        
+        {#if users.length === 0}
+            <div>There are no users</div>
+        {:else}
+            <div>
+                {#each users as user (user.username)}
+                    <div>{user.username}</div>
+                {/each}
+            </div>
+        {/if}
     </div>
 </div>
 
