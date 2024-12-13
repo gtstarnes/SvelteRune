d7b8f800c6bf18acb0936ad3a2f75b0f8bf14b1e
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-12-12 21:01:13 -0500

    bound values

--- src/routes/signup/signUp.svelte
+++ src/routes/signup/signUp.svelte
@@ -5,15 +5,15 @@
 <form>
     <div>
         <label for="name">Name</label>
-        <input type="text" id="name" name="name" />
+        <input type="text" id="name" name="name" bind:value={newUser.name}   />
     </div>
     <div>
         <label for="username">Username</label>
-        <input type="text" id="username" name="username" required />
+        <input type="text" id="username" name="username" bind:value={newUser.username} required />
     </div>
     <div>
         <label for="password">Password</label>
-        <input type="password" id="password" name="password" required />
+        <input type="password" id="password" name="password" bind:value={newUser.password} required />
     </div>
     <div>
         <button>Sign Up</button>
