c47ae0979a4e18f7c1a6d34f743149ebb869b0fe
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-12-15 23:48:58 -0500

    supplied html

--- src/routes/comments/components/comment.svelte
+++ src/routes/comments/components/comment.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
-
+const {text, likes}: {text: string, likes: number} = $props();
 </script>
 
 <div>
-
+    <div>
+        <div>{likes}</div>
+        <button>Like</button>
+    </div>
+    <div>{text}</div>
 </div>
 
 <style>
