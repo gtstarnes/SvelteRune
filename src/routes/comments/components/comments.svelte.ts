392a8028d2eea18ef0c11a33971478fa15561a69
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-12-15 23:28:03 -0500

    created add function

--- src/routes/comments/components/comments.svelte.ts
+++ src/routes/comments/components/comments.svelte.ts
@@ -1,8 +1,18 @@
 interface Comment {
     text: string,
-    author: string,
     likes: number,
     id: number
 }
 
 let comments = $state<Comment[]>([])
+
+function add(c: string) {
+    const newComment:Comment = {
+        text: c,
+        likes: 0,
+        id: Math.random(),
+    }
+    comments = [...comments, newComment]
+}
+
+export {add, }
