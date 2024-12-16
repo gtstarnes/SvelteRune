c57e20c8059497a4b444dae3ae0c19d139eb8391
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-12-15 23:58:26 -0500

    created control panel for comments

--- src/routes/comments/+page.svelte
+++ src/routes/comments/+page.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
 	import CommentList from "./components/commentList.svelte";
+	import Controls from "./components/controls.svelte";
 
 
 </script>
 
 <div>
+    <Controls />
     <CommentList />
 </div>
 
