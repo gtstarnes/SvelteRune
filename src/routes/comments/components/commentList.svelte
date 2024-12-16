e8e9b5758482f55fb10e619d52164f29d52c7dfa
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-12-15 23:54:13 -0500

    supplied html and conditional rendering

--- src/routes/comments/components/commentList.svelte
+++ src/routes/comments/components/commentList.svelte
@@ -1,12 +1,21 @@
 <script lang="ts">
-	import { getComments } from "./comments.svelte";
+	import Comment from "./comment.svelte";
+import { getComments } from "./comments.svelte";
 
 
     let comments = $derived(getComments())
 </script>
 
 <div>
-
+    {#if comments.length === 0}
+        <div>There are no comments</div>
+    {:else}
+        {#each comments as comment (comment.id)}
+            <ul>
+                <li><Comment {...comment} /></li>
+            </ul>
+        {/each}
+    {/if}
 </div>
 
 <style>
