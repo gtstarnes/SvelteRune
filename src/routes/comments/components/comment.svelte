7e6c407943f5a22689e1c68fbdcbb1950174c4ee
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-12-17 00:36:07 -0500

    added like function to buttons

--- src/routes/comments/components/comment.svelte
+++ src/routes/comments/components/comment.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
+	import { like } from "./comments.svelte";
+
 //VARIABLES
     let {text, count, id}: {text: string, count: number, id: number} = $props()
 </script>
 
 <div>
     <div>
-        <button>^</button>
+        <button onclick={() => like(id, "up")}>^</button>
         <span>{count}</span>
-        <button>v</button>
+        <button onclick={() => like(id, "down")}>v</button>
     </div>
     <div>{text}</div>
     <button>Delete</button>
