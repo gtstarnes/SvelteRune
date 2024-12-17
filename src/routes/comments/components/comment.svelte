701b5f74a0953d40bfb9d6541fb2eae6d2af35f9
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-12-17 00:10:54 -0500

    reset

--- src/routes/comments/components/comment.svelte
+++ src/routes/comments/components/comment.svelte
@@ -1,34 +1,11 @@
 <script lang="ts">
-	import { del, like } from "./comments.svelte";
 
-const {text, likes, id}: {text: string, likes: number, id: number} = $props();
 </script>
 
-<div class="container">
-    <div class="controls">
-        <div>{likes}</div>
-        <button onclick={()=>like(id)}>Like</button>
-    </div>
-    <div>{text}</div>
-    <button onclick={()=>del(id)}>X</button>
+<div>
+
 </div>
 
 <style>
-    .container {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        height: 3rem;
-        background-color: green;
-        padding: 0.5rem;
-    }
-    .controls {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        gap: 2px;
-    }
-    button {
-        cursor: pointer;
-    }
+
 </style>
