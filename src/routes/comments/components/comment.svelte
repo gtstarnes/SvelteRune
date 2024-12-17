fdc37d92be22d6a5212462be625c46ec2c1e560c
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-12-17 00:35:04 -0500

    created comment props; created basic html

--- src/routes/comments/components/comment.svelte
+++ src/routes/comments/components/comment.svelte
@@ -1,9 +1,16 @@
 <script lang="ts">
-
+//VARIABLES
+    let {text, count, id}: {text: string, count: number, id: number} = $props()
 </script>
 
 <div>
-
+    <div>
+        <button>^</button>
+        <span>{count}</span>
+        <button>v</button>
+    </div>
+    <div>{text}</div>
+    <button>Delete</button>
 </div>
 
 <style>
