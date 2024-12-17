aab05894862d21dce008eecfaff3b4d2c21ed913
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-12-17 00:26:19 -0500

    created basic html for control panel

--- src/routes/comments/components/controls.svelte
+++ src/routes/comments/components/controls.svelte
@@ -1,10 +1,15 @@
 <script lang="ts">
 
+//VARIABLES
+    let input = $state("")
+    let error = $state("")
 </script>
 
 <div>
-
+    <input type="text" placeholder="add comment" bind:value={input} />
+    <button>Add Comment</button>
 </div>
+<div>{error}</div>
 
 <style>
 
