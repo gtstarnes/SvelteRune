803d8a9fcd8df0dfc6ebe59cd46876996f4daa01
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-12-17 00:42:19 -0500

    supplied html for comment list

--- src/routes/comments/components/commentList.svelte
+++ src/routes/comments/components/commentList.svelte
@@ -1,9 +1,21 @@
 <script lang="ts">
+	import Comment from "./comment.svelte";
+    import { getComments } from "./comments.svelte";
 
+//VARIABLES
+    const comments = $derived(getComments())
 </script>
 
 <div>
-
+    {#if comments.length === 0}
+        <div>There are no comments, fool</div>
+    {:else}
+        <ul>
+            {#each comments as comment (comment.id)}
+                <li><Comment {...comment} /></li>
+            {/each}
+        </ul>
+    {/if}
 </div>
 
 <style>
