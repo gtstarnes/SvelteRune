f11119fd1aba5aad828dad210a0cd2e3d0d6290b
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-12-17 00:11:10 -0500

    reset

--- src/routes/comments/components/controls.svelte
+++ src/routes/comments/components/controls.svelte
@@ -1,21 +1,11 @@
 <script lang="ts">
-	import { add } from "./comments.svelte";
 
-    let input = $state("")
-
-    const check = () => {
-        if(input !== "") {
-            add(input)
-        }
-        input = ""
-    }
 </script>
 
 <div>
-    <input type="text" bind:value={input} />
-    <button onclick={check}>Add</button>
+
 </div>
 
 <style>
-    
+
 </style>
