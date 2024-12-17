f5271c895bf31e656fbf18a0803ee707301b3b5b
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-12-17 00:10:59 -0500

    reset

--- src/routes/comments/components/commentList.svelte
+++ src/routes/comments/components/commentList.svelte
@@ -1,25 +1,11 @@
 <script lang="ts">
-	import Comment from "./comment.svelte";
-import { getComments } from "./comments.svelte";
 
-
-    let comments = $derived(getComments())
 </script>
 
 <div>
-    {#if comments.length === 0}
-        <div>There are no comments</div>
-    {:else}
-        {#each comments as comment (comment.id)}
-            <ul>
-                <li><Comment {...comment} /></li>
-            </ul>
-        {/each}
-    {/if}
+
 </div>
 
 <style>
-    ul {
-        list-style: none;
-    }
+
 </style>
