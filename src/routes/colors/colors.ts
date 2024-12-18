9ddbe1058aa849f9cd31e2429eb3ea05faf1d725
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-12-18 18:54:59 -0500

    created color arrays

---
+++ src/routes/colors/colors.ts
@@ -0,0 +1,24 @@
+const color = [
+    {
+        color: "orange",
+        id: Math.random(),
+    },
+    {
+        color: "purple",
+        id: Math.random(),
+    },
+    {
+        color: "yellow",
+        id: Math.random(),
+    },
+    {
+        color: "pink",
+        id: Math.random(),
+    },
+    {
+        color: "gold",
+        id: Math.random(),
+    },
+]
+
+export { color } 
