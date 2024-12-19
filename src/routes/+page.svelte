699a7c39d438d4adfc3436121aa6d8065164c467
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-12-18 19:36:26 -0500

    refactored link list

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 //IMPORTS
 	import Link from "../components/Link.svelte";
+	import { links } from "../data/linkData";
 
 
 </script>
@@ -9,15 +10,9 @@
     <div>Mini Projects:</div>
     <div>
         <ul>
-            <li><Link text="Rune Count" href="/count" /></li>
-            <li><Link text="Rune Todo" href="/todo" /></li>
-            <li><Link text="Rune Area" href="/area" /></li>
-            <li><Link text="Rune Parent" href="/parent" /></li>
-            <li><Link text="Rune Binder" href="/binder" /></li>
-            <li><Link text="Rune Login" href="/login" /></li>
-            <li><Link text="Rune SignUp" href="/signup" /></li>
-            <li><Link text="Rune Comments" href="/comments" /></li>
-            <li><Link text="Rune Colors" href="/colors" /></li>
+            {#each links as link(link.href)}
+                <li><Link text={link.text} href={link.href}/></li>
+            {/each}
         </ul>
     </div>
 </div>
