1b0f240562f59650fa139a65fda329b4f35a2a68
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-12-18 19:55:29 -0500

    reset

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,16 +1,9 @@
 <script lang="ts">
-	import Login from "./login.svelte";
 
-    let details = $state({username: '', password:''})
 </script>
 
 <div>
-    {#if details.username === ''}
-        <div></div>
-    {:else}
-        <div>Hello: {details.username}</div>
-    {/if}
-    <Login  bind:details={details} />
+
 </div>
 
 <style>
