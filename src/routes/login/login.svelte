597059e472a2a1aa0c977834fbe8ce8de1789e5d
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-12-18 20:08:37 -0500

    bound values

--- src/routes/login/login.svelte
+++ src/routes/login/login.svelte
@@ -1,15 +1,17 @@
 <script lang="ts">
 
+//VARIABLE
+    let {username = $bindable(""), password = ""} = $props(); 
 </script>
 
 <form>
     <div>
-        <label for=""></label>
-        <input id="" name="" type="" />
+        <label for="username">Username: </label>
+        <input id="username" name="username" type="type" bind:value={username} />
     </div>
     <div>
-        <label for=""></label>
-        <input id="" name="" type="" />
+        <label for="password">Password:</label>
+        <input id="password" name="password" type="password" bind:value={password} />
     </div>
     <button>Login</button>
 </form>
