e471961c8e8eb1e6eca97d13ebd4e9e0da91960c
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-12-19 19:42:14 -0500

    created input variable

--- src/routes/login/login.svelte
+++ src/routes/login/login.svelte
@@ -4,19 +4,30 @@
         username: string,
         loggedIn: boolean
     } = $props()
+    let input = $state({
+        username: "",
+        password: ""
+    })
     let password = $state("")
     let error = $state("")
+
+    const handleSubmit = (e: SubmitEvent) => {
+        e.preventDefault()
+        if (username !== "" && password !== "") {
+            
+        }
+    }
 </script>
 
 <div>
     <form>
         <span>
             <label for="username">Username: </label>
-            <input type="text" id="username" name="username" bind:value={username} />
+            <input type="text" id="username" name="username" bind:value={input.username} />
         </span>
         <span>
             <label for="password">Password: </label>
-            <input type="password" id="password" name="password" bind:value={password} />
+            <input type="password" id="password" name="password" bind:value={input.password} />
         </span>
         <div>{error}</div>
         <button>Submit</button>
