2d0b31e59b06a18f3e75af9b5453eb0b9b63f72a
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-12-19 19:58:07 -0500

    created logout function

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -3,18 +3,20 @@
 
     let {username, loggedIn} = $state({username:"", loggedIn: false})
 
+    const logOut = () => {
+        username = ""
+        loggedIn = false
+    }
 </script>
 
 <div>
-    <Login bind:username  bind:loggedIn/>
     {#if username === ''}
         <Login bind:username  bind:loggedIn/>
         <div></div>
     {:else}
-        <button>Log Out</button>
+        <button onclick={logOut}>Log Out</button>
         <div>HELLO: {username.toUpperCase()}</div>
     {/if}
-    
 </div>
 
 
