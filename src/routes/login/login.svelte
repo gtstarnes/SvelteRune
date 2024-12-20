efd2ee6e8c950e6485edb33fd870526491cc93d2
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-12-19 19:39:24 -0500

    created error variable

--- src/routes/login/login.svelte
+++ src/routes/login/login.svelte
@@ -5,17 +5,18 @@
         loggedIn: boolean
     } = $props()
     let password = $state("")
+    let error = $state("")
 </script>
 
 <div>
     <form>
         <span>
             <label for="username">Username: </label>
-            <input type="text" id="username" name="username" />
+            <input type="text" id="username" name="username" bind:value={username} />
         </span>
         <span>
             <label for="password">Password: </label>
-            <input type="password" id="password" name="password" />
+            <input type="password" id="password" name="password" bind:value={password} />
         </span>
         <button>Submit</button>
     </form>
