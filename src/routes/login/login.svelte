f13d7fab801352ec6f2d5db553ecb0356467f56b
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-12-19 19:33:47 -0500

    added login form html

--- src/routes/login/login.svelte
+++ src/routes/login/login.svelte
@@ -3,7 +3,17 @@
 </script>
 
 <div>
-
+    <form>
+        <span>
+            <label for="username">Username: </label>
+            <input type="text" id="username" name="username" />
+        </span>
+        <span>
+            <label for="password">Password: </label>
+            <input type="password" id="password" name="password" />
+        </span>
+        <button>Submit</button>
+    </form>
 </div>
 
 <style>
