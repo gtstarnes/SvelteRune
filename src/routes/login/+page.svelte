10c6f5e4191f2c9e46e5bdd819b53613c9f75388
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-12-19 19:57:14 -0500

    moved login inside conditional rendering; added button to else statement

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -7,6 +7,14 @@
 
 <div>
     <Login bind:username  bind:loggedIn/>
+    {#if username === ''}
+        <Login bind:username  bind:loggedIn/>
+        <div></div>
+    {:else}
+        <button>Log Out</button>
+        <div>HELLO: {username.toUpperCase()}</div>
+    {/if}
+    
 </div>
 
 
