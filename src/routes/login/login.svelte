d836d8db0b5e495172a5c154efa0f8533a8e2a09
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-12-19 19:29:19 -0500

    reset

--- src/routes/login/login.svelte
+++ src/routes/login/login.svelte
@@ -1,34 +1,9 @@
 <script lang="ts">
 
-//VARIABLE
-    let {username = $bindable("")} = $props(); 
-    let input = $state({
-        username: "",
-        password: "",
-    })
-
-    const handleSubmit = (e: SubmitEvent) => {
-        e.preventDefault()
-        username = input.username
-        input = {
-            username: "",
-            password: "",
-        }
-    }
 </script>
 
 <div>
-    <form onsubmit={handleSubmit}>
-        <div>
-            <label for="username">Username: </label>
-            <input id="username" name="username" type="type" bind:value={input.username} />
-        </div>
-        <div>
-            <label for="password">Password:</label>
-            <input id="password" name="password" type="password" bind:value={input.password} />
-        </div>
-        <button>Login</button>
-    </form>
+
 </div>
 
 <style>
