8cdc87a069485bd91e2365307a5937504c39be40
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-12-20 20:42:40 -0500

    reset

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,22 +1,9 @@
 <script lang="ts">
-	import Login from "./login.svelte";
 
-    let {username, loggedIn} = $state({username:"", loggedIn: false})
-
-    const logOut = () => {
-        username = ""
-        loggedIn = false
-    }
 </script>
 
 <div>
-    {#if username === ''}
-        <Login bind:username  bind:loggedIn/>
-        <div></div>
-    {:else}
-        <div>HELLO: {username.toUpperCase()}</div>
-        <button onclick={logOut}>Log Out</button>
-    {/if}
+
 </div>
 
 
