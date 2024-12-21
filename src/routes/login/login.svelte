f4d474d0479ed239f05ac369c222bb81f5fb7213
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-12-20 20:48:14 -0500

    created html skeleton

--- src/routes/login/login.svelte
+++ src/routes/login/login.svelte
@@ -3,7 +3,17 @@
 </script>
 
 <div>
-
+    <form>
+        <span>
+            <label for="Username">Username: </label>
+            <input type="text" id="username" name="username" />
+        </span>
+        <span>
+            <label for="password">Password: </label>
+            <input type="password" id="password" name="password" />
+        </span>
+        <button type="submit">Login</button>
+    </form>
 </div>
 
 <style>
