0c687782d5490ce5a3d870aee7e6e5bde2e2b295
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-12-20 20:43:08 -0500

    reset

--- src/routes/login/login.svelte
+++ src/routes/login/login.svelte
@@ -1,51 +1,9 @@
 <script lang="ts">
-//VARIABLES
-    let {username = $bindable(""), loggedIn = $bindable(false)}: {
-        username: string,
-        loggedIn: boolean
-    } = $props()
-    let input = $state({
-        username: "",
-        password: ""
-    })
-    let error = $state("")
 
-    const handleSubmit = (e: SubmitEvent) => {
-        e.preventDefault()
-        if (input.username !== "" && input.password !== "") {
-            username = input.username
-            loggedIn = true
-            error = ""
-        } else {
-            handleError()
-        }
-        input = {username: '', password: ''}
-    }
-
-    const handleError = () => {
-        if (input.username === '' && input.password === ''){
-            error = "Username and password are required"
-        } else if (input.username === ""){
-            error = "Username is required"
-        } else {
-            error = "Password is required"
-        }
-    }
 </script>
 
 <div>
-    <form onsubmit={handleSubmit}>
-        <span>
-            <label for="username">Username: </label>
-            <input type="text" id="username" name="username" bind:value={input.username} />
-        </span>
-        <span>
-            <label for="password">Password: </label>
-            <input type="password" id="password" name="password" bind:value={input.password} />
-        </span>
-        <div>{error}</div>
-        <button type="submit">Submit</button>
-    </form>
+
 </div>
 
 <style>
