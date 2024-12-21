2cfb6b1a2fa657f9d85b899d1cc9f83b1add4e8f
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-12-20 21:05:00 -0500

    bound values improved readability

--- src/routes/login/login.svelte
+++ src/routes/login/login.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
 //VARIABLES
-    let {username = $bindable(""), loggedIn = $bindable(false)} = $props();
-    let input = $state({username: '', password: '', userError: '', passError:''});
+    let {
+        username = $bindable(""),
+        loggedIn = $bindable(false)
+    } = $props()
+    let input = $state({
+        username: '', password: '', 
+        userError: '', passError:'',
+    })
 
 </script>
 
@@ -9,16 +15,18 @@
     <form>
         <span>
             <label for="Username">Username: </label>
-            <input type="text" id="username" name="username" />
-            <span></span>
+            <input type="text" id="username" 
+                name="username" bind:value={input.username} />
+            <span>{input.userError}</span>
         </span>
         <span>
             <label for="password">Password: </label>
-            <input type="password" id="password" name="password" />
+            <input type="password" id="password" 
+                name="password" bind:value={input.password} />
+            <span>{input.passError}</span>
         </span>
         <button type="submit">Login</button>
     </form>
-
 </div>
 
 <style>
