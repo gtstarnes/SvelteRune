16f34a36b245a8575d89fc76b748c8b99d74ea55
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-12-23 22:22:28 -0500

    created useeffect to conditionally render based on browser size

--- src/routes/a/hamburger/+page.svelte
+++ src/routes/a/hamburger/+page.svelte
@@ -6,25 +6,45 @@
 //VARIABLES
 const navLinks = ["Home", "About", "Projects"]
 let active = $state("Home")
-
+let width = $state(0)
 //FUNCTIONS
 function changeActive(link: string) {
     if (link !== active) {
         active = link
     }
 }
+
+//EFFECTS
+$effect(() => {
+    if (typeof window !== "undefined") {
+        const handleResize = () => {
+            width = window.innerWidth;
+        }
+        window.addEventListener('resize', handleResize)
+
+        return () => {
+            window.removeEventListener('resize', handleResize)
+        }
+    }
+})
+
+
 </script>
 
 <div>
     <nav>
-        <ul>
-            {#each navLinks as link (link)}
-                <li class:active={link === active} onclick={() => changeActive(link)}>{link}</li>
-            {/each}
-        </ul>    
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-            <path fill="currentColor" d="M2 15.5v2h20v-2zm0-5v2h20v-2zm0-5v2h20v-2z"/>
-        </svg>
+        {#if width < 768}
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                <path fill="currentColor" d="M2 15.5v2h20v-2zm0-5v2h20v-2zm0-5v2h20v-2z"/>
+            </svg>
+        {:else}
+            <ul>
+                {#each navLinks as link (link)}
+                    <li class:active={link === active} onclick={() => changeActive(link)}>{link}</li>
+                {/each}
+            </ul>    
+        {/if}
+
     </nav>
 
 </div>
