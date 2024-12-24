f38b98274da31a6db25147c21605534dff3dd7b1
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-12-23 21:24:42 -0500

    created basic html

--- src/routes/a/hamburger/+page.svelte
+++ src/routes/a/hamburger/+page.svelte
@@ -3,9 +3,15 @@
 </script>
 
 <div>
-
+    <nav>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Projects</li>
+        </ul>
+    </nav>
 </div>
 
 <style>
-    
+
 </style>
