24f1613cd8f0b82ab80cb9cd9658644fff9cfe93
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-12-23 22:28:37 -0500

    reset

--- src/routes/count/+page.svelte
+++ src/routes/count/+page.svelte
@@ -6,59 +6,13 @@
  *      center everything
  *      style
  */
-//VARIABLES
-    let count = $state<number>(0)
-    let countSquared = $derived<number>(count*count)
 
-//FUNCTIONS
-    const add = () => {
-        count++
-    }
-    const reset = () => {
-        count = 0
-    }
-    const sub = () => {
-        count--
-    }
 </script>
 
-<div class="container">
-    <div class="count">{count}</div>
-    <div>
-        <button class="math" onclick={add}>+</button>
-        <button class="reset" onclick={reset}>Reset</button>
-        <button class="math" onclick={sub}>-</button>
-    </div>
-    <div class="count">{countSquared}</div>
+<div >
+
 </div>
 
 <style>
-    .container {
-        height: 90vh;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-    }
-    .count {
-        font-size: 6rem;
-    }
-    .math {
-        background-color: orange;
-        font-weight: bold;
-        font-size: 2rem;
-    }
-    button {
-        height: 3rem;
-        width: 6rem;
-        border: none;
-        border-radius: 2%;
-        cursor: pointer;
-        font-size: 1rem;
-    }
-    .math:active {
-        background-color: darkorange;
-        scale: 95%;
-    }
 
 </style>
