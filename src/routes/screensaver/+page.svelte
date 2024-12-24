188d9d880e179fb3fe4b18c937c7e5ee8c32af43
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-12-23 22:57:27 -0500

    played around with offset

--- src/routes/screensaver/+page.svelte
+++ src/routes/screensaver/+page.svelte
@@ -14,5 +14,16 @@
         display: flex;
         justify-content: center;
         align-items: center;
+        offset: path("M 100 100 L 200 600 L 300 100 L 500 600 z") auto;
+        animation: move 3s linear infinite;
+    }
+
+    @keyframes move {
+        from {
+            offset-distance: 0%;
+        }
+        to {
+            offset-distance: 100%;
+        }
     }
 </style>
