29a9aab26b91e02895d50ed9be82bec0cdf678fa
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-12-26 16:44:33 -0500

    reset

--- src/routes/a/login/+page.svelte
+++ src/routes/a/login/+page.svelte
@@ -2,28 +2,11 @@
 /**
  * Rep: 3
 */
-	import Login from "./login.svelte";
 
-//VARIABLES
-    let username = $state('');
-    let loggedIn = $state(false);
-
-//FUNCTIONS
-    const logOut = () => {
-        username = ''
-        loggedIn = false
-    }
 </script>
 
 <div>
-    {#if loggedIn === false}
-        <Login bind:username bind:loggedIn />
-    {:else}
-        <div>
-            <span>Hello, {username}</span>
-            <button onclick={logOut}>Log Out</button>
-        </div>
-    {/if}
+
 </div>
 
 
