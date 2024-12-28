1f172860bd37f24f1554ce36fd64a78b83610197
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-12-27 20:30:59 -0500

    reset

--- src/routes/a/login/+page.svelte
+++ src/routes/a/login/+page.svelte
@@ -3,41 +3,13 @@
  * Rep: 4
 */
 
-	import Login from "./login.svelte";
-
-//VARIABLES
-let {username, loggedIn} = $state({
-    username: '',
-    loggedIn: false,
-})
-
-function handleLogOut() {
-    username = ''
-    loggedIn = false
-}
 </script>
 
-<div class="container">
-    {#if loggedIn === false}
-        <Login bind:username bind:loggedIn />
-    {:else}
-        <div class="message">
-            <span>Hello, {username}</span>
-            <button onclick={handleLogOut}>Log Out</button>
-        </div>
-    {/if}
+<div>
+
 </div>
 
 
 <style>
-    .container {
-        display: flex;
-        width: 98vw;
-        height: 90vh;
-        justify-content: center;
-        align-items: center;
-    }
-    .message {
-        font-size: 3rem;
-    }
+
 </style>
--- src/routes/a/login/login.svelte
+++ src/routes/a/login/login.svelte
@@ -4,69 +4,13 @@
  * Rep: 4
  */
 
- //VARIABLES
-let {username = $bindable(""), loggedIn = $bindable(false)} = $props();
-let input = $state({
-    username: '',
-    password: '',
-    error: ''
-})
 
-function handleSubmit(e: SubmitEvent) {
-    e.preventDefault()
-    if(isError()){
-        username = input.username
-        loggedIn = true
-    }
-    resetInput()
-}
-function isError() {
-    let safe = false
-    if (input.username === '' && input.password === ''){
-        input.error = 'Username and Password are required'
-    } else if (input.username === ''){
-        input.error = 'Username is required'
-    } else if (input.password === ''){
-        input.error = 'Password is required'
-    } else {
-        input.error = ''
-        safe = true
-    }
-    return safe
-}
-function resetInput() {
-    input.username = ''
-    input.password = ''
-}
 </script>
 
-<div class="container">
-    <form onsubmit={handleSubmit}>
-        <span>
-            <label for="username">Username:</label>
-            <input type="text" id="username" name="username" bind:value={input.username} />
-        </span>
-        <span>
-            <label for="password">Password:</label>
-            <input type="password" id="password" name="password" bind:value={input.password} />
-        </span>
-        <button type="submit">Login</button>
-        <span>{input.error}</span>
-    </form>
+<div>
     
 </div>
 
 <style>
-    .container {
-        width: 100%;
-        height: 90vh;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
-    form {
-        display: flex;
-        flex-direction: column;
-        gap: 1rem;
-    }
+
 </style>
