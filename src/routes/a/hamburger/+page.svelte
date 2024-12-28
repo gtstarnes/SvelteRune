550cd503a3048ab5faa476e638dd974d40d363a6
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-12-27 21:46:08 -0500

    created changeTab function

--- src/routes/a/hamburger/+page.svelte
+++ src/routes/a/hamburger/+page.svelte
@@ -2,9 +2,16 @@
 /**
  * REP: 2
 */
+//VARIABLES
 const tabs = ['Home', 'About', 'Projects',] 
 let activeTab = $state("Home")
 
+//FUNCTIONS
+function changeTab (tab: string) {
+    if (tab !== activeTab) {
+        activeTab = tab
+    }
+}
 
 </script>
 
