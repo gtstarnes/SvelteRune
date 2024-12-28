3d9baee0b7f8e6bc562813f149512cc6f62f3b9a
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-12-27 22:03:11 -0500

    moved code

--- src/routes/a/hamburger/+page.svelte
+++ src/routes/a/hamburger/+page.svelte
@@ -2,45 +2,15 @@
 /**
  * REP: 2
 */
-//VARIABLES
-const tabs = ['Home', 'About', 'Projects',] 
-let activeTab = $state("Home")
 
-//FUNCTIONS
-function changeTab (tab: string) {
-    if (tab !== activeTab) {
-        activeTab = tab
-    }
-}
+	import Nav from "./nav.svelte";
 
 </script>
 
 <div>
-    <nav>
-        <div>Company Logo</div>
-        <ul>
-            {#each tabs as tab (tab)}
-                <li><button onclick={()=> changeTab(tab)}>{tab}</button></li>
-            {/each}
-        </ul>
-    </nav>
+    <Nav />
 </div>
 
 <style>
-    nav {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-    }
-    ul {
-        display: flex;
-        list-style: none;
-    }
-    button {
-        background-color: inherit;
-        border: inherit;
-        font-style: inherit;
-        cursor: pointer;
-        width: 50px;
-    }
+
 </style>
