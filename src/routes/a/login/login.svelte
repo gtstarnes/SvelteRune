eed74258f93d641792ca8010ce18cf1a36f081f9
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-12-27 20:34:13 -0500

    created basic html

--- src/routes/a/login/login.svelte
+++ src/routes/a/login/login.svelte
@@ -8,7 +8,17 @@
 </script>
 
 <div>
-    
+    <form>
+        <div>
+            <label for=""></label>
+            <input type="" id="" name="" />
+        </div>
+        <div>
+            <label for=""></label>
+            <input type="" id="" name="" />
+        </div>
+        <button>Login</button>
+    </form>
 </div>
 
 <style>
