1814d373abab0418c0d2ada305988df1545db1fa
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-12-27 21:43:32 -0500

    reset

--- src/routes/a/hamburger/+page.svelte
+++ src/routes/a/hamburger/+page.svelte
@@ -3,79 +3,15 @@
  * REP: 2
 */
 
-	import Hamburger from "./hamburger.svelte";
 
-//VARIABLES
-const tabs = ['Home', 'About', 'Projects']
-let active = $state('Home');
-let width = $state(0)
 
-//FUNCTIONS
-function changeTab(tab: string) {
-    if (tab !== active){
-        active = tab
-    }
-}
-
-//EFFECTS
-$effect(() => {
-    function handleResize() {
-        if (typeof window !== 'undefined') {
-            width = window.innerWidth
-        }
-    }
-    window.addEventListener('resize', handleResize)
-
-    return () => {
-        window.removeEventListener('resize', handleResize)
-    }
-    
-})
 
 </script>
 
 <div>
-    <nav>
-        <span>COMPANY</span>
-        <span class="logo">LOGO</span>
-        {#if width < 1024}
-            <Hamburger />
-        {:else}
-            <ul>
-                {#each tabs as tab (tab)}
-                    <li onclick={() => changeTab(tab)}
-                        class:active={tab === active}>{tab}</li>
-                {/each}
-            </ul>
-        {/if}
-    </nav>
 
 </div>
 
 <style>
-    nav {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        height: 4rem;
-    }
-    ul {
-        display: flex;
-        list-style: none;
-        gap: 2px;
-    }
-    li {
-        width: 4rem;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        cursor: pointer;
-    }
-    .active {
-        color: pink;
-        border-bottom: 1px solid pink;
-    }
-    .logo {
-        justify-self: center;
-    }
+
 </style>
