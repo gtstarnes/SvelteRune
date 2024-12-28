f9664001a6b032f726827b84863ebe1578365bd7
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-12-27 21:19:20 -0500

    created user list

---
+++ src/routes/a/login/users.ts
@@ -0,0 +1,14 @@
+export const users = [
+    {
+        username: 'AzzClappa',
+        password: '1234',
+    },
+    {
+        username: 'DixiEnormous',
+        password: '4321',
+    },
+    {
+        username: 'BobDucca',
+        password: 'password',
+    },
+]
