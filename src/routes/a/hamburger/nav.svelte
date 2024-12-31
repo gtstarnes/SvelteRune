e674d84c9068e258ee602203ad826cb04951cdc7
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-12-30 21:46:29 -0500

    added use effect for window resize

--- src/routes/a/hamburger/nav.svelte
+++ src/routes/a/hamburger/nav.svelte
@@ -2,19 +2,36 @@
     /**
      * REP: III
     */
-import Hamburger from "./hamburger.svelte";
+    import Hamburger from "./hamburger.svelte";
 
-    
-
+    //VARIABLES
     const links = ["Home", "About", "Projects"]
     let {active = $bindable('Home')} = $props()
     let width = $state(0);
 
+    //FUNCTIONS
     function changeActive(a: string) {
         if (a !== active) {
             active = a
         }
     }
+
+    //EFFECTS 
+    $effect(() => {
+       
+        function resize() { 
+            if (typeof window !== 'undefined'){
+                width = window.innerWidth
+            }
+        }
+
+        window.addEventListener('resize', resize)
+
+        return () => {
+            window.removeEventListener('resize', resize)
+        }
+       
+    })
     </script>
     
     <div>
