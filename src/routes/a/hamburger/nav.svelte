23a94f337d38700a4bebb2a2299cd92d8c7330dd
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-12-30 21:27:07 -0500

    created styles for button

--- src/routes/a/hamburger/nav.svelte
+++ src/routes/a/hamburger/nav.svelte
@@ -12,7 +12,7 @@
             <div>
                 <ul>
                     {#each links as link (link)}
-                        <li>{link}</li>
+                        <li><button>{link}</button></li>
                     {/each}
                 </ul>
 
@@ -25,5 +25,15 @@
             display: flex;
             justify-content: space-between;
             align-items: center;
+        }
+        ul {
+            list-style: none;
+            display: flex;
+            gap: .5rem;
+        }
+        button {
+            border: none;
+            background-color: inherit;
+            cursor: pointer;
         }
     </style>
