e7c5517a849920c3069fff017709c4cc5657790c
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-12-30 21:03:49 -0500

    create for each to display colors

--- src/routes/colors/+page.svelte
+++ src/routes/colors/+page.svelte
@@ -1,9 +1,16 @@
 <script lang="ts">
+/**
+ * REPS: I
+*/
+
+	import { colors } from "./colors";
 
 </script>
 
 <div>
-
+    {#each colors as color (color.id)}
+        <div>{color.color}</div>
+    {/each}
 </div>
 
 <style>
