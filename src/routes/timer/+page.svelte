f00c559fc8a3bfbd8fdd578687fae32072a0d40e
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-01-03 00:02:45 -0500

    created timer toggle

--- src/routes/timer/+page.svelte
+++ src/routes/timer/+page.svelte
@@ -2,6 +2,11 @@
     let timer = $state(0)
     let mins = $derived(Math.floor(timer/60));
     let seconds = $derived(timer - mins * 60);
+    let isRunning = $state(false);
+
+    function toggleTimer() {
+        isRunning = true;
+    }
 
 </script>
 
@@ -21,6 +26,7 @@
         <button onclick={() => timer = timer+60}>+01:00</button>
         <button onclick={() => timer = timer+300}>+05:00</button>
     </div>
+    <button onclick={toggleTimer}>Start</button>
 </div>
 
 <style>
