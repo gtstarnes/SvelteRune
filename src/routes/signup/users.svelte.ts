49235cbe6b47d38e391ee7e871e23b01c8584278
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-01-02 21:01:09 -0500

    added names

--- src/routes/signup/users.svelte.ts
+++ src/routes/signup/users.svelte.ts
@@ -1,19 +1,23 @@
 interface User {
     username: string,
+    name: string
     password: string,
 }
 
 const initial = [
     {
         username: 'AzzClappa',
+        name: "cacasdcsac",
         password: '1234',
     },
     {
         username: 'DixiEnormous',
+        name: "cacasdcsac",
         password: '4321',
     },
     {
         username: 'BobDucca',
+        name: "cacasdcsac",
         password: 'password',
     },
 ]
