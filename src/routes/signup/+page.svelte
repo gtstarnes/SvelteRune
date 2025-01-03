be00456569458dce91bcd48dba6ec5767966b40f
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-01-02 21:03:31 -0500

    reset

--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -1,22 +1,13 @@
 <script lang="ts">
-	import SignUp from "./signUp.svelte";
-	import { users } from "./users.svelte";
+/**
+ * REP: I
+*/
+
 
 </script>
 
 <div>
-    <SignUp />
-    <div>
-        {#if users.length === 0}
-            <div>There are no users</div>
-        {:else}
-            <div>
-                {#each users as user (user.username)}
-                    <div>{user.username}</div>
-                {/each}
-            </div>
-        {/if}
-    </div>
+
 </div>
 
 <style></style>
--- src/routes/signup/signUp.svelte
+++ src/routes/signup/signUp.svelte
@@ -1,36 +1,12 @@
 <script lang="ts">
-	import { users } from "./users.svelte";
+/**
+ * REP: I
+*/
 
-    let newUser = $state({name: "", username: "", password: ""})
-
-    const handleSubmit = (e: Event) => {
-        e.preventDefault
-        users.push(newUser)
-        newUser = {name: "", username: "", password: ""}
-    }
-
-    const reset = () => {
-        newUser = {name: "", username: "", password: ""}
-    }
 </script>
 
-<form onsubmit={handleSubmit}>
-    <div>
-        <label for="name">Name</label>
-        <input type="text" id="name" name="name" bind:value={newUser.name}   />
-    </div>
-    <div>
-        <label for="username">Username</label>
-        <input type="text" id="username" name="username" bind:value={newUser.username} required />
-    </div>
-    <div>
-        <label for="password">Password</label>
-        <input type="password" id="password" name="password" bind:value={newUser.password} required />
-    </div>
-    <div>
-        <button>Sign Up</button>
-        <button onclick={reset}>Reset</button>
-    </div>
+<form>
+
 </form>
 
 <style></style>
