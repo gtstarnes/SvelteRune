6bd717c5b9b4a3202ac1913f672b9a1ada116f3b
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-01-02 23:55:20 -0500

    create timer counter

--- src/routes/timer/+page.svelte
+++ src/routes/timer/+page.svelte
@@ -1,13 +1,25 @@
 <script lang="ts">
+    let timer = $state(0)
+    let mins = $derived(Math.floor(timer/60));
+    let seconds = $derived(timer - mins * 60);
 
 </script>
 
 <div>
-    <span></span>
+    <span>
+        {#if mins < 10}
+            0{mins}
+        {:else}
+            {mins}
+        {/if}:{#if seconds < 10}0{seconds}
+        {:else}
+            {seconds}
+        {/if}
+    </span>
     <div>
-        <button>+00:30</button>
-        <button>+01:00</button>
-        <button>+05:00</button>
+        <button onclick={() => timer = timer+30}>+00:30</button>
+        <button onclick={() => timer = timer+60}>+01:00</button>
+        <button onclick={() => timer = timer+300}>+05:00</button>
     </div>
 </div>
 
