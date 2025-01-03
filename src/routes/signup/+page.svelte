9b533404cdb9a144e7b5be6a65a1d38fa1aea7bb
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-01-02 21:05:14 -0500

    created display for users

--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -4,12 +4,20 @@
 */
 
 	import SignUp from "./signUp.svelte";
+	import { users } from "./users.svelte";
 
 
 </script>
 
 <div>
     <SignUp />
+    <div>
+        <ul>
+            {#each users as user (user.username)}
+                <li>{user.username}</li>
+            {/each}
+        </ul>
+    </div>
 </div>
 
 <style></style>
