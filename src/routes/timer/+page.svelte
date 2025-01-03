b29032bb5c9f6c2167b33dc2653ca2e5e514b837
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-01-02 23:42:58 -0500

    added html

--- src/routes/timer/+page.svelte
+++ src/routes/timer/+page.svelte
@@ -3,9 +3,14 @@
 </script>
 
 <div>
-
+    <span></span>
+    <div>
+        <button>+00:30</button>
+        <button>+01:00</button>
+        <button>+05:00</button>
+    </div>
 </div>
 
 <style>
-    
+
 </style>
