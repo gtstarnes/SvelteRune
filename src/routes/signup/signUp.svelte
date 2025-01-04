0b27f05abac73cc6e08a5acce2ac33a24de1b237
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-01-03 20:20:30 -0500

    reset

--- src/routes/signup/signUp.svelte
+++ src/routes/signup/signUp.svelte
@@ -1,93 +1,14 @@
 <script lang="ts">
-	import { getUsers, addUser } from "./users.svelte";
-
 /**
  * REP: I
 */
-let details = $state({
-    name: '',
-    username: '',
-    password: '',
-    error: '',
-})
-const users = $derived(getUsers());
 
-
-//FUNCTIONS
-function handleSubmit(e: SubmitEvent) {
-    e.preventDefault
-    if (!checkError() && checkUsers()){
-        const newUser  = {username: details.username, name: details.name, password: details.password}
-        addUser(newUser);
-    }
-    resetForm()
-}
-function checkError(){
-    let err = true
-
-    if (details.username === '' && details.password === ''){
-        details.error = 'cannot submit empty form'
-    } else if (details.username === ''){
-        details.error = 'username is required'
-    } else if (details.password === ''){
-        details.error = 'password is required'
-    } else {
-        err = false;
-        details.error = '';
-    }
-
-    return err
-}
-function checkUsers(){
-    let err = true
-    const found = users.find(user => {
-        return user.username === details.username
-    })
-    if (found) {
-        err = false
-        details.error = "username already exists"
-    }
-    return err
-}
-function resetForm(){
- details.name = details.username = details.password = ''
-}
 </script>
 
-<form onsubmit={handleSubmit}>
-    <span>{details.error}</span>
-    <span class="input">
-        <label for="name">Name</label>
-        <input bind:value={details.name} id="name" name="name" type="text" />
-    </span>
-    <span class="input">
-        <label for="username">Username</label>
-        <input bind:value={details.username} id="username" name="username" type="text" placeholder="(required)" />
-    </span>
-    <span class="input">
-        <label for="password">Password</label>
-        <input bind:value={details.password} id="password" name="password" type="password" placeholder="(required)" />
-    </span>
-    <button type="submit">Sign Up</button>
+<form>
+    
 </form>
 
 <style>
-    form {
-        display: flex;
-        flex-direction: column;
-        width: 40vw;
-        gap: 0.25rem;
-    }
-    .input {
-        display: flex;
-        justify-content: space-between;
-    }
-    input {
-        text-align: end;
-        width: 70%;
-        padding-right: 8px;
-    }
-    button {
-        cursor: pointer;
-    }
+ 
 </style>
