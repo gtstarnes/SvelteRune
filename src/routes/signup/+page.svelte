6f31e6e9212b85bb91ea5b6d4b73f59da3e8e489
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-01-03 19:57:51 -0500

    reset

--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -3,37 +3,12 @@
  * REP: I
 */
 
-	import SignUp from "./signUp.svelte";
-	import { getUsers } from "./users.svelte";
-
-const users = $derived(getUsers());
 </script>
 
 <div class="container">
-    <SignUp />
-    <div>
-        <ul>
-            {#each users as user (user.username)}
-                <li>{user.username}</li>
-            {/each}
-        </ul>
-    </div>
+
 </div>
 
 <style>
-    .container {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        width: 99vw;
-        height: 90vh;
-    }
-    ul {
-        list-style: none;
-        display: grid;
-        grid-template-columns: 33% 33% 33%;
-        width: 99vw;
-        text-align: center;
-    }
+
 </style>
