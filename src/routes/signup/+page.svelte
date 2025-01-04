1ff1e989848deeb55f7a610da189a8508f4d1737
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-01-03 20:13:25 -0500

    imported login and sign up

--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -3,10 +3,14 @@
  * REP: I
 */
 
+	import Login from "./login.svelte";
+	import SignUp from "./signUp.svelte";
+
 </script>
 
 <div class="container">
-
+    <Login />
+    <SignUp />
 </div>
 
 <style>
