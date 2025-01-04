67b775ee2b75fb9c3b8ac1423c9e149c634a1e8e
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-01-03 20:16:44 -0500

    imported getUser

--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -5,12 +5,15 @@
 
 	import Login from "./login.svelte";
 	import SignUp from "./signUp.svelte";
+	import { getUsers } from "./users.svelte";
 
+const users = $derived(getUsers())
 </script>
 
 <div class="container">
     <Login />
     <SignUp />
+    
 </div>
 
 <style>
