28edc64a7d553567bdc55836c9579ed8884684a7
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-01-03 22:02:08 -0500

    created input variables

--- src/routes/signup/login.svelte
+++ src/routes/signup/login.svelte
@@ -4,17 +4,22 @@
     let  {username = $bindable(''), 
     loggedIn = $bindable(false),
     portal = $bindable(true)} = $props();
+    let input = $state({
+        username: '',
+        password: '',
+        error: ''
+    })
 </script>
 
 <form>
-    <span></span>
+    <span>{input.error}</span>
     <span>
         <label for="username">Username</label>
-        <input id='username' name='username' type=''  />
+        <input id='username' name='username' type='' bind:value={input.username}  />
     </span>
     <span>
         <label for="password">Password</label>
-        <input id='password' name='password' type='password'  />
+        <input id='password' name='password' type='password' bind:value={input.password}  />
     </span>
     <div>
         <button>Log In</button>
