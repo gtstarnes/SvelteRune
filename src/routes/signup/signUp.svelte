1553317cba29b3343ad0dfe0635cd0443ec36f26
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-01-03 20:22:34 -0500

    added html

--- src/routes/signup/signUp.svelte
+++ src/routes/signup/signUp.svelte
@@ -6,7 +6,17 @@
 </script>
 
 <form>
-    
+    <span></span>
+    <span>
+        <label for=""></label>
+        <input name="" id="" type="" />
+    </span>
+    <span>
+        <label for=""></label>
+        <input name="" id="" type="" />
+    </span>
+    <button>Submit</button>
+    <button>Login</button>
 </form>
 
 <style>
