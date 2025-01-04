147a8d613a256cec2f1c410c1a1c694b46fe37d4
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-01-03 20:32:07 -0500

    created details state and bound values

--- src/routes/signup/signUp.svelte
+++ src/routes/signup/signUp.svelte
@@ -2,21 +2,32 @@
 /**
  * REP: I
 */
-
+let details = $state({
+    name: '',
+    username: '',
+    password: '',
+    error: '',
+})
 </script>
 
 <form>
-    <span></span>
+    <span>{details.error}</span>
     <span>
-        <label for=""></label>
-        <input name="" id="" type="" />
+        <label for="name">Name</label>
+        <input name="name" id="name" type="" bind:value={details.name} />
     </span>
     <span>
-        <label for=""></label>
-        <input name="" id="" type="" />
+        <label for="username">Username</label>
+        <input name="username" id="username" type="username" bind:value={details.username} />
     </span>
-    <button>Submit</button>
-    <button>Login</button>
+    <span>
+        <label for="password">Password</label>
+        <input name="password" id="password" type="password" bind:value={details.password} />
+    </span>
+    <div>
+        <button>Submit</button>
+        <button>Login</button>
+    </div>
 </form>
 
 <style>
