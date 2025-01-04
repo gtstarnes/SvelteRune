208e0ff10de18e47c833b7b0bbd5571b1638bdb1
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-01-03 22:36:42 -0500

    bound variables to login

--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -33,7 +33,7 @@ function handleLogOut() {
         </ul>
     {:else}
         {#if portal === true}
-            <Login bind:portal />
+            <Login bind:username bind:loggedIn bind:portal />
         {:else}
             <SignUp bind:username bind:loggedIn bind:portal />
         {/if}
