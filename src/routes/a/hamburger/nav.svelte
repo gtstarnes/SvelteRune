b161038d20b2428cac00b0472f84fc2c68f0ae56
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-01-06 18:06:20 -0500

    supplied change tab function

--- src/routes/a/hamburger/nav.svelte
+++ src/routes/a/hamburger/nav.svelte
@@ -5,20 +5,25 @@
     let tabs = $state(["Home", "About", "Projects"]);
     let {active = $bindable("Home")} = $props();
 
-    </script>
+    function changeTab(t: string){
+        if (t !== active) {
+            active === t
+        }
+    }
+</script>
 
   
-    <div>
-        <span>LOGO</span>
-        <nav>
-            <ul>
-                {#each tabs as tab (tab)}
-                    <li><button>{tab}</button></li>
-                {/each}
-            </ul>
-        </nav>
-    </div>
+<div>
+    <span>LOGO</span>
+    <nav>
+        <ul>
+            {#each tabs as tab (tab)}
+                <li><button onclick={() => changeTab(tab)}>{tab}</button></li>
+            {/each}
+        </ul>
+    </nav>
+</div>
     
-    <style>
+<style>
 
-    </style>
+</style>
