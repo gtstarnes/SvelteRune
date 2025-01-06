e18bbf4aeb1f1959a40655a85a4394c6e692a5f3
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-01-06 17:59:10 -0500

    reset

--- src/routes/a/hamburger/nav.svelte
+++ src/routes/a/hamburger/nav.svelte
@@ -16,24 +16,5 @@
     </div>
     
     <style>
-        nav {
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-        }
-        ul {
-            list-style: none;
-            display: flex;
-            gap: .5rem;
-        }
-        button {
-            border: none;
-            background-color: inherit;
-            cursor: pointer;
-            width: 10vw;
-        }
-        .active {
-            font-weight: bold;
-            color: gold;
-        }
+
     </style>
