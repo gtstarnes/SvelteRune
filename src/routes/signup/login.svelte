3c772c289a5b545ac94be07f07dfc58ef524529d
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-01-06 20:56:56 -0500

    add function skeletons

--- src/routes/signup/login.svelte
+++ src/routes/signup/login.svelte
@@ -4,20 +4,35 @@
     */
 
 let {portal = $bindable()} = $props()
-
+let input = $state({
+    username: '',
+    password: '',
+    error: '',
+})
 function togglePortal() {
     portal = false
+}
+
+function handleSubmit(){
+
+}
+function checkErrors(){
+
+}
+function resetInput(){
+
 }
 </script>
 
 <form>
+    <span>{input.error}</span>
     <span>
         <label for="username">Username</label>
-        <input name="username" id="username" type="text" />
+        <input name="username" id="username" type="text" bind:value={input.username} />
     </span>
     <span>
         <label for="password">Password</label>
-        <input name="password" id="password" type="password" />
+        <input name="password" id="password" type="password" bind:value={input.password} />
     </span>
     <div>
         <button type="submit">Login</button>
