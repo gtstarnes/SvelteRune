7cf4f4f654e7528165ee8c3c8cc2a10eedd985ea
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-01-06 19:50:30 -0500

    imported and displayed users

--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -1,12 +1,20 @@
 <script lang="ts">
+	import { getUsers } from "./users.svelte";
+
 /**
  * REP: I
 */
 
-
+    const users = $derived(getUsers())
 </script>
 
-
+<div class="container">
+    <ul>
+        {#each users as user (user.username)}
+            <li>{user.username}</li>
+        {/each}
+    </ul>
+</div>
 
 <style>
 
