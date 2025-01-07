bc2409c61d9ec1d25a54770cfdb05aabe0aab0a9
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-01-06 20:06:17 -0500

    bound values

--- src/routes/signup/signUp.svelte
+++ src/routes/signup/signUp.svelte
@@ -10,18 +10,18 @@ let input = $state({
 </script>
 
 <form>
-    <span></span>
+    <span>{input.error}</span>
     <span>
         <label for="name">Name</label>
-        <input name="name" id="name" type="text" />
+        <input name="name" id="name" type="text" bind:value={input.name} />
     </span>
     <span>
         <label for="username">Username</label>
-        <input name="username" id="username" type="text" />
+        <input name="username" id="username" type="text" bind:value={input.username} />
     </span>
     <span>
         <label for="password">Password</label>
-        <input name="password" id="password" type="password" />
+        <input name="password" id="password" type="password" bind:value={input.password} />
     </span>
     <div>
         <button type="submit">Sign Up</button>
