6b04952acf92c386cf3f4c3cca1771aee49167be
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-01-06 19:59:18 -0500

    created conditional rendering

--- src/routes/signup/portal.svelte
+++ src/routes/signup/portal.svelte
@@ -1,10 +1,17 @@
 <script lang="ts">
+	import Login from "./login.svelte";
+import SignUp from "./signUp.svelte";
+
 
     let portal = $state(false)
 </script>
 
 <div>
-    
+    {#if portal === false}
+        <SignUp />
+    {:else}
+        <Login />
+    {/if}
 </div>
 
 <style>
