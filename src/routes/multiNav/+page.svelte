1b2a698a7b075ece71b5ee463525b5bbcd809a72
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-01-07 21:03:47 -0500

    added styles

--- src/routes/multiNav/+page.svelte
+++ src/routes/multiNav/+page.svelte
@@ -5,17 +5,17 @@
         <ul>
             <li><button>Home</button></li>
             <li>
-                <div>
+                <nav class='subnav'>
                     <button>Project</button>
-                    <nav>
-                        <ul>
+                    <div class="subnav-content">
+                        <ul >
                             <li><button>adc</button></li>
                             <li><button>342</button></li>
                             <li><button>fsf</button></li>
                             <li><button>45g</button></li>
                         </ul>
-                    </nav>
-                </div>
+                    </div>
+                </nav>
             </li>
             <li><button>About</button></li>
         </ul>
@@ -23,4 +23,28 @@
 </div>
 
 
-<style></style>
+<style>
+    ul {
+        display: flex;
+        align-items: center;
+        list-style: none;
+    }
+    .subnav {
+        float: left;
+        overflow:hidden;
+        width: 100%;
+    }
+    .subnav-content{
+        position: absolute;
+        left: 0;
+        width:100%;
+        display: none;
+    }
+    .subnav:hover .subnav-content {
+        display: block;
+    }
+    button {
+        background-color: inherit;
+        border: none;
+    }
+</style>
