75171894c2e9fe415cc519aff08241215e7e1588
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-01-07 19:48:09 -0500

    added html

--- src/routes/todo/+page.svelte
+++ src/routes/todo/+page.svelte
@@ -4,11 +4,30 @@
      */
 
 	import Controls from "./controls.svelte";
+	import { getTasks } from "./todo.svelte";
 
+    const tasks = $derived(getTasks())
 </script>
 
 <div>
     <Controls />
+    <div>
+        {#if tasks.length === 0}
+            There are no tasks
+        {:else}
+            <ul>
+                {#each tasks as task(task)}
+                    <li>
+                        <div>{task.text}</div>
+                        <span>
+                            <button>Edit</button>
+                            <button>Delete</button>
+                        </span>
+                    </li>
+                {/each}
+            </ul>
+        {/if}
+    </div>
 </div>
 
 <style>
