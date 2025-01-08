16c9ce3d646de0aa2ff772ed17d23e8b4a970c34
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-01-07 19:28:05 -0500

    created input variable

--- src/routes/todo/controls.svelte
+++ src/routes/todo/controls.svelte
@@ -1,7 +1,10 @@
-<script lang="ts"></script>
+<script lang="ts">
+
+    let input = $state('');
+</script>
 
 <form>
-    <input type="text" placeholder="add task..." />
+    <input type="text" placeholder="add task..." bind:value={input} />
     <span>
         <button type="submit">Add</button>
         <button type="button">Mass Delete</button>
