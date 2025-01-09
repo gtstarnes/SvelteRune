0178de938712a44d1fbaa2924775f20201849473
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-01-08 22:04:35 -0500

    moved code

--- src/routes/a/hamburger/+page.svelte
+++ src/routes/a/hamburger/+page.svelte
@@ -3,68 +3,15 @@
  * REP: V
 */
 
-	import Hamburger from "./hamburger.svelte";
+	import Nav from "./nav.svelte";
 
-    //VARIABLES
-    const tabs = ["Home", "About", "Projects", ]
-    let {active = $bindable("Home")} = $props()
-    let width = $state(0);
-
-    //FUNCTIONS
-    function changeTab(tab:string) {
-        if (tab !== active){
-            active = tab
-        }
-    }
 </script>
 
-<svelte:window bind:innerWidth={width} />
+
 <div class="container">
-    <div>LOGO</div>
-    {#if width > 600}
-        <nav>
-            <ul>
-                {#each tabs as tab(tab)}
-                    <li class:active={tab === active}>
-                        <button 
-                        onclick={() => changeTab(tab)}>{tab}</button></li>
-                {/each}
-            </ul>
-        </nav>
-    {:else}
-        <Hamburger />
-    {/if}
+    <Nav />
 </div>
 
 <style>
-    .container {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        background-color: grey;
-        padding-left: 1rem;
-        padding-right: 1rem;
-    }
-    ul {
-        display: flex;
-        list-style: none;
-        height: 100%;
-        margin: 0;
-    }
-    button {
-        height: 100%;
-        background-color: inherit;
-        border: none;
-        height: 2rem;
-        width: 4rem;
-        padding-top: 6px;
-    }
-    button:hover, li:hover {
-        background-color: blueviolet;
-        cursor: pointer;
-    }
-    .active {
-        border-bottom: 4px solid blueviolet;
-    }
 
 </style>
