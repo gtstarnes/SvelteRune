d11850711556d48a5a43a89e2dc58b8260422777
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-01-08 21:21:31 -0500

    reset

--- src/routes/b/count/+page.svelte
+++ src/routes/b/count/+page.svelte
@@ -8,65 +8,13 @@
  *      commit after task blocks
  */
 
-//VARIABLES
-let count = $state(0)
-let squared = $derived(count * count)
 
-//FUNCTIONS
-function add() {
-    count++
-}
-function minus(){
-    count--
-}
-function reset(){
-    count = 0
-}
 </script>
 
 <div class="container">
-    <span>{count}</span>
-    <div class="controls">
-        <button class="math" onclick={add}>+</button>
-        <button class="reset" onclick={reset}>Reset</button>
-        <button class="math" onclick={minus}>-</button>
-    </div>
-    <span>{squared}</span>
+
 </div>
 
 <style>
-    .container {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        height: 90vh;
-    }
-    .controls {
-        height: 8rem;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        gap: 0.5rem;
-    }
-    .math {
-        background-color: orange;
-    }
-    .math:active {
-        background-color: darkorange;
-        scale: 95%;
-    }
-    .reset {
-        margin-top: 20px;
-    }
-    button {
-        height: 4rem;
-        width: 8rem;
-        font-size: 2rem;
-        cursor: pointer;
-        border: none;
-    }
-    span{
-        font-size: 6rem;
-    }
+
 </style>
