6020e6d8bccf9d1068daabc3f1f3153335a2f6c4
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-01-08 21:43:19 -0500

    created varaibles

--- src/routes/a/hamburger/+page.svelte
+++ src/routes/a/hamburger/+page.svelte
@@ -2,14 +2,16 @@
 /**
  * REP: V
 */
-
+    //VARIABLES
+    const tabs = ["Home", "About", "Projects", ]
+    let {active = $bindable("Home")} = $props()
 </script>
 
 <div class="container">
     <div>LOGO</div>
     <nav>
         <ul>
-            
+
         </ul>
     </nav>
 </div>
