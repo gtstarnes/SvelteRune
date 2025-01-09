d08429aefffef9d17ffa228309ef0ff65a039a2f
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-01-08 21:40:04 -0500

    reset

--- src/routes/a/hamburger/nav.svelte
+++ src/routes/a/hamburger/nav.svelte
@@ -1,58 +1,14 @@
 <script lang="ts">
-	import Hamburger from "./hamburger.svelte";
-
     /**
      * REP: V
     */
-    let tabs = $state(["Home", "About", "Projects"]);
-    let {active = $bindable("Home")} = $props();
-    let width = $state(0)
 
-    function changeTab(t: string){
-        if (t !== active) {
-            active = t
-        }
-       
-    }
 </script>
 
-<svelte:window bind:innerWidth={width} />
 <div>
-    <span>LOGO</span>
-    {#if width >= 1040}
-        <nav>
-            <ul>
-                {#each tabs as tab (tab)}
-                    <li><button onclick={() => changeTab(tab)}
-                        class:active={tab === active}>{tab}</button></li>
-                {/each}
-            </ul>
-        </nav>
-    {:else}
-        <Hamburger />
-    {/if}
+
 </div>
     
 <style>
-    div {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-    }
-    ul {
-        display: flex;
-        list-style: none;
-    }
-    li {
-        margin-right: 0.5em;
-    }
-    button {
-        background-color: inherit;
-        border: none;
-        cursor: pointer;
-    }
-    .active {
-        color: pink;
-        border-bottom: 2px solid pink;
-    }
+
 </style>
