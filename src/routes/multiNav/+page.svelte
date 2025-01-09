b9bdefe8648ef4edc0ce806297d216569f4634e7
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-01-08 20:21:20 -0500

    fixed styles

--- src/routes/multiNav/+page.svelte
+++ src/routes/multiNav/+page.svelte
@@ -7,28 +7,45 @@
 <div class="container">
     <nav class="main">
         <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Projects</li>
+            <li><button>Home</button></li>
+            <li><button>About</button></li>
+            <li><button>Projects</button></li>
         </ul>
     </nav>
 </div>
 
 
 <style>
+    * {
+        border: 1px solid black
+    }
     .container {
         display: flex;
-        justify-content: end;
+        justify-content: center;
         align-items: center;
+        height: 90vh;
     }
     .main {
         background-color: grey;
         width: 100%;
         display: flex;
         justify-content: end;
+        padding-right: 2rem;
     }
     ul {
         display: flex;
         list-style: none;
+        height: inherit;
+        margin: 0;
+    }
+    button {
+        background-color: inherit;
+        border: none;
+        cursor: pointer;
+        height: 4rem;
+        width: 6rem;
+    }
+    button:hover {
+        background-color: blueviolet;
     }
 </style>
