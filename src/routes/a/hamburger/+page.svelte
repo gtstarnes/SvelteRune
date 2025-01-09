cd44cf9094dfd9f3bc5960f2703217c618aa4059
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-01-08 22:07:24 -0500

    created variables and added conditional rendering

--- src/routes/a/hamburger/+page.svelte
+++ src/routes/a/hamburger/+page.svelte
@@ -5,13 +5,24 @@
 
 	import Nav from "./nav.svelte";
 
+    //VARIABLES
+    let active = $state("Home")
 </script>
 
 
 <div class="container">
-    <Nav />
+    <Nav bind:active />
+    <section>
+        {#if active === "Home"}
+            <span>welcome.</span>
+        {:else if active === "About"}
+            <span>about.</span>
+        {:else}
+            <span>projects.</span>
+        {/if}
+    </section>
 </div>
 
 <style>
-
+    
 </style>
