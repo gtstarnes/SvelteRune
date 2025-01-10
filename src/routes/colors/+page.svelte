003dcbd424bdf80ed1ba85ae57565914a7ebe4c6
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-01-09 22:54:24 -0500

    supplied html

--- src/routes/colors/+page.svelte
+++ src/routes/colors/+page.svelte
@@ -3,12 +3,22 @@
  * REPS: II
 */
 
+	import { colors } from "./colors";
+
 
 
 </script>
 
 <div class="container">
-
+    <div>
+        <ul>
+            {#each colors as color (color.id)}
+                <li>
+                    <div>{color.color}</div>
+                </li>
+            {/each}
+        </ul>
+    </div>
 </div>
 
 <style>
