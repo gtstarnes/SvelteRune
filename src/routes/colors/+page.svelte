c90b81759b8b65a7cc93d69ac9d2f228be22e960
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-01-09 22:52:47 -0500

    reset

--- src/routes/colors/+page.svelte
+++ src/routes/colors/+page.svelte
@@ -3,45 +3,14 @@
  * REPS: II
 */
 
-	import { colors } from "./colors";
+
 
 </script>
 
 <div class="container">
-    <div class="row">
-        {#each colors as color (color.id)}
-            <div class="block" style={`background-color: ${color.color}`}>{color.color}</div>
-        {/each}
-    </div>
+
 </div>
 
 <style>
-    .container {
-        display: flex;
-        width: 100vw;
-        height: 90vh;
-        justify-content: center;
-        align-items: center;
-    }
-    .row {
-        display: flex;
-        width: 100vw;
-        justify-content: center;
-        align-items: center;
-        height: 20vh;
-        gap: 2%;
-    }
-    .block {
-        width: 10vw;
-        height: 14vh;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        cursor: pointer;
-        transition: width 0.50s ease-out;
-    }
-    .block:hover {
-        width: 18vw;
-        transition: width 0.25s ease-in;
-    }
+
 </style>
