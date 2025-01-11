b5c16678c7dc9384596222c4dbe8c363dd934283
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-01-10 20:03:15 -0500

    added html

--- src/routes/multiNav/+page.svelte
+++ src/routes/multiNav/+page.svelte
@@ -5,7 +5,32 @@
 </script>
 
 <div class="container">
-
+    <span>LOGO</span>
+    <nav>
+        <ul>
+            <li><button>Home</button></li>
+            <li>
+                <nav>
+                    <button>About</button>
+                    <ul>
+                        <li><button>A</button></li>
+                        <li><button>B</button></li>
+                        <li><button>C</button></li>
+                    </ul>
+                </nav>
+            </li>
+            <li>
+                <nav>
+                    <button>Project</button>
+                    <ul>
+                        <li><button>D</button></li>
+                        <li><button>E</button></li>
+                        <li><button>F</button></li>
+                    </ul>
+                </nav>
+            </li>
+        </ul>
+    </nav>
 </div>
 
 
