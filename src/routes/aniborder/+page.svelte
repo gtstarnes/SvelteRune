76dde4e196c828065977076d40ac2071f485a7b8
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-01-13 19:53:20 -0500

    initial create

--- src/routes/aniborder/+page.svelte
+++ src/routes/aniborder/+page.svelte
@@ -4,16 +4,7 @@
 
 
 <div class="container">
-    <div class='card'>
-        <h3>ANIBORDER</h3>
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-    </div>
+
 </div>
 
 
@@ -25,25 +16,4 @@
     align-items: center;
     height: 90vh; 
 }
-.card {
-    width: 18rem;
-    text-align: center;
-    padding: 3em;
-    border-radius: 10px;
-    position: relative;
-    background-color: black;
-    
-}
-.card::after {
-    content: '';
-    position: absolute;
-    height: 100%;
-    width: 100%;
-    background-color: gold;
-    inset: 0;
-    z-index: -1;
-    padding: 3px;
-    
-}
-
 </style>
