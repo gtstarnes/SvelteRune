17d2c660f396087b2af72aeb5a63c7ee2470e17a
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-01-13 20:44:25 -0500

    added toggled and styles

--- src/routes/dark/button.svelte
+++ src/routes/dark/button.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
 
+const toggle = () => {
+        window.document.body.classList.toggle('dark-mode')
+    }
 </script>
 
 <button>
@@ -7,5 +10,15 @@
 </button>
 
 <style>
-
+    button {
+        background-color: blueviolet;
+        border: none;
+        height: 2.5rem;
+        width: 5rem;
+        cursor: pointer;
+    }
+    :global(body.dark-mode) button {
+        background-color: antiquewhite;
+        color: blueviolet
+    }
 </style>
