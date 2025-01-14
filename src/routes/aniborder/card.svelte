fd87b7821177b7ae21ed6b13194dfa1f5dafd387
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-01-13 20:23:17 -0500

    didn't work

--- src/routes/aniborder/card.svelte
+++ src/routes/aniborder/card.svelte
@@ -11,8 +11,28 @@
         Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
         Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
     </p>
+    <div class='border'></div>
 </div>
 
 <style>
-
+    .card {
+        padding: 2em;
+        width: 18rem;
+        height: 18rem;
+        background-color: #1c1f2b;
+        text-align: center;
+        border-radius: 10px;
+        position: relative;
+        z-index: 1;
+    }
+    .border {
+        content: '';
+        position: absolute;
+        height: 100%;
+        width: 100%;
+        background-color: gold;
+        inset: 0;
+        z-index: -1;
+        padding: 7px;
+    }
 </style>
