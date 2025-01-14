9975b28c8d7fc962b5c4bdc1f5a73ae73857ee84
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-01-13 20:59:04 -0500

    added styles

--- src/routes/dark/+page.svelte
+++ src/routes/dark/+page.svelte
@@ -4,9 +4,27 @@
 
 </script>
 
-<h1>Word</h1>
-<Button />
+<div>
+    <h1>Word</h1>
+    <Button />
+</div>
 
 <style>
-
+ :global(body){
+    margin: 0;
+    padding: 1rem;
+    background-color: white;
+    transition: background-color 0.5s
+ }
+ :global(body.dark-mode) {
+    background-color: black;
+    color: blueviolet
+ }
+ div {
+    display: flex;
+    flex-direction: column;
+    height: 90.2vh;
+    justify-content: center;
+    align-items: center;
+ }
 </style>
