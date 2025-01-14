00c2ded06fcc578688376d442739a4402c35b7c1
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-01-13 21:26:09 -0500

    added dark mode toggle to header

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
+	import Button from "../routes/dark/button.svelte";
+
 
 </script>
 
 
 <header>
     <div>Svelte Rune</div>
-    <a href="/">Home</a>
+    <span>
+        <Button />
+        <a href="/">Home</a>
+    </span>
 </header>
 
 
