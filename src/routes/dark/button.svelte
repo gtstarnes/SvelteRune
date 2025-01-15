291caea857c615c71ebcc35755f450a77c59cde8
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-01-14 22:51:46 -0500

    reset

--- src/routes/dark/button.svelte
+++ src/routes/dark/button.svelte
@@ -2,33 +2,12 @@
 /**
  * REP: !
  */
-const toggle = () => {
-        window.document.body.classList.toggle('dark-mode')
-    }
+
+
 </script>
 
-<button onclick={toggle}>
-    Click meh
-</button>
+
 
 <style>
-    button {
-        background-color: blueviolet;
-        border: none;
-        height: 2.5rem;
-        width: 5rem;
-        cursor: pointer;
-        color: white;
-    }
-    :global(body){
-    margin: 0;
-    padding: 1rem;
-    background-color: white;
-    transition: background-color 0.5s
-    }
-    :global(body.dark-mode) {
-        background-color: black;
-        color: blueviolet
-    }
 
 </style>
