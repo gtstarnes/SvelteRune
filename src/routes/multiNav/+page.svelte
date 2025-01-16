a8065d18d5e97853739e158cce0cf95eb454ab0a
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-01-15 21:26:57 -0500

    reset

--- src/routes/multiNav/+page.svelte
+++ src/routes/multiNav/+page.svelte
@@ -5,91 +5,11 @@
 </script>
 
 <div class="container">
-    <div class="header">
-        <span>Logo</span>
-        <input type="text" />
-        <nav class="main">
-            <ul>
-                <li><button>Home</button></li>
-                <li>
-                    <nav class="subnav">
-                        <button>About</button>
-                        <ul class="subnav-content">
-                            <li><button>A</button></li>
-                            <li><button>B</button></li>
-                            <li><button>C</button></li>
-                        </ul>
-                    </nav>
-                </li>
-                <li>
-                    <nav class="subnav">
-                        <button>Projects</button>
-                        <ul class="subnav-content">
-                            <li><button>D</button></li>
-                            <li><button>E</button></li>
-                            <li><button>F</button></li>
-                        </ul>
-                    </nav>
-                </li>
-            </ul>
-        </nav>
-    </div>
+
 </div>
 
 
 <style>
-    .container {
-        height: 90vh;
 
-    }
-    .header {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        background-color: grey;
-        padding-left: 0.5rem;
-    }
-    .main {
-        position: relative;
-    }
-    .subnav-content {
-        position: absolute;
-        right: 0;
-        visibility: hidden;
-        background-color: blueviolet;
-    }
-    .subnav:hover .subnav-content {
-        visibility: visible;
-    }
-    .main button:hover, .subnav:hover {
-        background-color: blueviolet;
-    }    
-    .subnav-content button:hover {
-        background-color: grey;
-    }
-    ul {
-        display: flex;
-        list-style: none;
-        margin: 0;
-        padding: 0;
-    }
-    button {
-        border: none;
-        background-color: inherit;
-        color: whitesmoke;
-        cursor: pointer;
-        height: 2rem;
-        width: 4rem;
-    }
-    input {
-        border: 2px solid blueviolet;
-        border-radius: 8px;
-        width: 50%;
-        padding-left: 1rem;
-        caret-color: blueviolet;
-    }
-    input[type=text]:focus {
-        accent-color: blueviolet;
-    }
 
 </style>
