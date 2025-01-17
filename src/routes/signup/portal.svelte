b2248fb2629f55278e45e7d848ad6fe66ad93156
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-01-16 23:09:28 -0500

    removed variable

--- src/routes/signup/portal.svelte
+++ src/routes/signup/portal.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
 	import Login from "./login.svelte";
-import SignUp from "./signUp.svelte";
+    import SignUp from "./signUp.svelte";
 	import { getPortal } from "./users.svelte";
 
-let {loggedIn = $bindable(false)} = $props();
+
 let portal = $derived(getPortal());
 </script>
 
