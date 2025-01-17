63762037477456e47cfcb6de15b3977b1bd4c81f
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-01-16 22:12:28 -0500

    reset

--- src/routes/signup/portal.svelte
+++ src/routes/signup/portal.svelte
@@ -1,20 +1,9 @@
 <script lang="ts">
-	import Login from "./login.svelte";
-import SignUp from "./signUp.svelte";
 
-    let {
-        username = $bindable(), 
-        loggedIn = $bindable(),
-    } = $props()
-    let portal = $state(false)
 </script>
 
 <div>
-    {#if portal === false && loggedIn === false}
-        <SignUp bind:username bind:loggedIn bind:portal />
-    {:else}
-        <Login bind:username bind:loggedIn bind:portal />
-    {/if}
+
 </div>
 
 <style>
