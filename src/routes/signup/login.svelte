006056669771c166ee8410279ce31f04dfe05528
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-01-16 22:12:24 -0500

    reset

--- src/routes/signup/login.svelte
+++ src/routes/signup/login.svelte
@@ -1,74 +1,18 @@
 <script lang="ts">
-	import { checkPassword, findUser } from "./users.svelte";
+
 
     /**
      * REP: I
     */
 
-let {username = $bindable(''), loggedIn = $bindable(false), portal = $bindable(true)} = $props()
-let input = $state({
-    username: '',
-    password: '',
-    error: '',
-})
-function togglePortal() {
-    portal = false
-}
 
-function handleSubmit(e: SubmitEvent){
-    e.preventDefault
-    checkErrors()
-    if (input.error === ''){
-        username = input.username
-        loggedIn = true
-    }
-    resetInput()
-}
-function checkErrors(){
-    switch(true){
-        case input.username === '' && input.password === '':
-            return input.error = 'username and password are required' 
-        case input.username === '':
-            return input.error = 'username is required'
-        case input.password === '':
-            return input.error = 'password is required'
-        case !findUser(input.username):
-            return input.error = 'username not found'
-        case !checkPassword(input.username, input.password):
-            return input.error = 'password is not correct'
-        default:
-            return input.error = ''
-    }
-}
-function resetInput(){
-    input.username = ''
-    input.password = ''
-}
 </script>
 
-<form onsubmit={handleSubmit}>
-    <span>{input.error}</span>
-    <span>
-        <label for="username">Username</label>
-        <input name="username" id="username" type="text" bind:value={input.username} />
-    </span>
-    <span>
-        <label for="password">Password</label>
-        <input name="password" id="password" type="password" bind:value={input.password} />
-    </span>
-    <div>
-        <button type="submit">Login</button>
-        <button type="button" onclick={togglePortal}>Signup</button>
-    </div>
+<form>
+
 </form>
 
 
 <style>
-    form {
-        display: flex;
-        flex-direction: column;
-    }
-    button {
-        cursor: pointer;
-    }
+
 </style>
