f102793de6e679a635b0e30b2bd14bc6f881ce2c
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-01-16 23:11:14 -0500

    bound variables

--- src/routes/signup/login.svelte
+++ src/routes/signup/login.svelte
@@ -18,12 +18,13 @@
 <form>
     <span>
         <label for="username">Username:</label>
-        <input name="username" id="username" type="text"/>
+        <input name="username" id="username" type="text" bind:value={details.username}/>
     </span>
     <span>
         <label for="password">Password:</label>
-        <input name="password" id="password" type="password"/>
+        <input name="password" id="password" type="password" bind:value={details.password}/>
     </span>
+    <div>{details.error}</div>
     <div>
         <button type="submit">Log In</button>
         <button type="button" onclick={togglePortal}>Sign Up</button>
