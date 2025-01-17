3e94e84d0c4be39ac2d42bd69beddd16ab06912c
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-01-16 23:08:38 -0500

    fixed portal varaible

--- src/routes/signup/portal.svelte
+++ src/routes/signup/portal.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
 	import Login from "./login.svelte";
 import SignUp from "./signUp.svelte";
+	import { getPortal } from "./users.svelte";
 
 let {loggedIn = $bindable(false)} = $props();
-let portal = $state(false);
+let portal = $derived(getPortal());
 </script>
 
 <div>
