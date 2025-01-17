63caf665e3fc8bdd36d786892466a6c9485eaddc
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-01-16 22:17:38 -0500

    added conditional rendering

--- src/routes/signup/portal.svelte
+++ src/routes/signup/portal.svelte
@@ -1,10 +1,17 @@
 <script lang="ts">
+	import Login from "./login.svelte";
+import SignUp from "./signUp.svelte";
+
 let {loggedIn = $bindable(false)} = $props();
 let portalToggle = $state(false);
 </script>
 
 <div>
-
+    {#if portalToggle}
+        <SignUp />
+    {:else}
+        <Login />
+    {/if}
 </div>
 
 <style>
