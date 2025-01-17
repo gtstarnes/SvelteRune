f83703e253bda0cafd8280347a2b8da75bc0e1ba
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-01-16 22:13:58 -0500

    added container styles

--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -12,5 +12,11 @@
 </div>
 
 <style>
-
+    .container {
+        height: 90vh;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
 </style>
