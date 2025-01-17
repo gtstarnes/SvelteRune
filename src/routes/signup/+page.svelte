228acd912970be75827e2e0c879b7551408de3e4
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-01-16 22:12:18 -0500

    reset

--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -1,35 +1,13 @@
 <script lang="ts">
-	import Portal from "./portal.svelte";
-import { getUsers } from "./users.svelte";
+
 
 /**
  * REP: I
 */
 
-    const users = $derived(getUsers());
-    let username = $state('')
-    let loggedIn = $state(false)
-
-    function logOut() {
-        loggedIn = false
-        username = ''
-    }
 </script>
 
 <div class="container">
-    {#if loggedIn === false}
-        <Portal bind:username bind:loggedIn/>
-    {:else}
-        <div>
-            {username}
-            <button onclick={logOut}>Log Out</button>
-        </div>
-        <ul>
-            {#each users as user (user.username)}
-                <li>{user.username}</li>
-            {/each}
-        </ul>
-    {/if}
 
 </div>
 
