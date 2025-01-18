ce174a5b5a218c2272b3944123dfe941cd04a965
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-01-17 19:15:32 -0500

    reset

--- src/routes/b/count/+page.svelte
+++ src/routes/b/count/+page.svelte
@@ -9,47 +9,14 @@
  */
 
 //variables
-    let count = $state(0)
-    let square = $derived(count * count)
+
 </script>
 
 <div class="container">
-    <div class="count">{count}</div>
-    <div>
-        <button class="math" onclick={() => count++}>+</button>
-        <button class="clear" onclick={() => count = 0}>clr</button>
-        <button class="math" onclick={() => count--}>-</button>
-    </div>
-    <div class="count">{square}</div>
+
 </div>
 
 <style>
-    .container {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        height: 90vh;
-    }
-    .count {
-        font-size: 6rem;
-    }
-    button {
-        width: 5rem;
-        height: 3rem;
-        font-size: 2rem;
-        cursor: pointer;
-        border: none;
-    }
-    .math {
-        background-color: orange;
-    }
-    .math:active {
-        background-color: darkorange;
-        scale: 95%;
-    }
-    .clear {
-        transform: translateY(0.35rem);
-    }
+
 
 </style>
