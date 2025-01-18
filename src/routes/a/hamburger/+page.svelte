4e35a0ee2e9dc7a123e9598e02de5d0c995c1978
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-01-17 19:38:46 -0500

    reset

--- src/routes/a/hamburger/+page.svelte
+++ src/routes/a/hamburger/+page.svelte
@@ -3,37 +3,13 @@
  * REP: VI
 */
 
-	import Nav from "./nav.svelte";
-
-    //VARIABLES
-    let active = $state("Home")
 </script>
 
 
 <div class="container">
-    <Nav bind:active />
-    <section>
-        {#if active === "Home"}
-            <span>welcome.</span>
-        {:else if active === "About"}
-            <span>about.</span>
-        {:else}
-            <span>projects.</span>
-        {/if}
-    </section>
+
 </div>
 
 <style>
-    .container {
-        height:90vh;
-    }
-    section {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        height: 100%;
-    }
-    span {
-        font-size: 8rem;
-    }
+
 </style>
