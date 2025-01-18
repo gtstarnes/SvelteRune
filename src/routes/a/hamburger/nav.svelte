019f421269c57e26eaab7d1ab137466d8c2373e0
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-01-17 20:08:05 -0500

    displayed nav

--- src/routes/a/hamburger/nav.svelte
+++ src/routes/a/hamburger/nav.svelte
@@ -3,14 +3,29 @@
      * REP: VI
     */
 
-    const tabs = ['Home', 'About', 'Projects'];
+    const tabs = ['Home', {main: 'About', sub:['A', 'B', 'C']}, {main: 'Projects', sub:['D', 'E', 'F']}];
     let active = $state('Home');
 </script>
 
 
-<div class="container">
-
-</div>
+<nav class="container">
+    <ul>
+        {#each tabs as tab (tab)}
+            {#if typeof tab === 'object'}
+                <nav>
+                    <li><button>{tab.main}</button></li>
+                    <div>
+                        {#each tab.sub as sub}
+                            <li><button>{sub}</button></li>
+                        {/each}
+                    </div>
+                </nav>
+            {:else}
+                <li><button>{tab}</button></li>
+            {/if}
+        {/each}
+    </ul>
+</nav>
 
 <style>
 
