5bd516f3d0da900f1858a671cc8186d01504d3a4
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-01-20 22:16:25 -0500

    added button styles

--- src/routes/dark/button.svelte
+++ src/routes/dark/button.svelte
@@ -3,12 +3,20 @@
  * REP: II
  */
 
+	import Sun from "./sun.svelte";
+
 
 
 </script>
 
-
+<button><Sun /></button>
 
 <style>
-
+    button {
+        border: none;
+        background-color: inherit;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
 </style>
