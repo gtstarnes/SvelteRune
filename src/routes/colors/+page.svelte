ae3cd87115826f2df8b4907b6f5c45afeadb4b92
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-01-20 21:49:48 -0500

    reset

--- src/routes/colors/+page.svelte
+++ src/routes/colors/+page.svelte
@@ -10,38 +10,9 @@
 </script>
 
 <div class="container">
-    <div>
-        <ul>
-            {#each colors as color(color.id)}
-                <li><button style={`background-color: ${color.color}`}>{color.color}</button></li>
-            {/each}
-        </ul>
-    </div>
+
 </div>
 
 <style>
-    .container {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        height: 90vh;
-    }
-    ul {
-        list-style: none;
-        display: flex;
-        gap: 4px;
-    }
-    button {
-        height: 7rem;
-        width: 5rem;
-        padding: 1rem;
-        border:none;
-        text-align: center;
-        cursor: pointer;
-        transition: width 0.7s;
-    }
-    button:hover{
-        width: 8rem;
-    }
+
 </style>
