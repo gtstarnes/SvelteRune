3648a6a684eafb4585262a9e2d83c245a6fa9f57
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-01-20 21:50:21 -0500

    reset

--- src/routes/dark/button.svelte
+++ src/routes/dark/button.svelte
@@ -3,19 +3,12 @@
  * REP: II
  */
 
- const toggle = () => {
-    document.querySelector("body")?.classList.toggle("dark-mode")
- }
+
 
 </script>
 
-<button onclick={toggle}>(=)</button>
+
 
 <style>
-    button {
-        background-color: inherit;
-        color: inherit;
-        border:none;
-        cursor: pointer;
-    }
+
 </style>
