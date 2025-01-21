51a0e274982cf7a9640a4b1fd3f70a02730ba105
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-01-20 22:57:54 -0500

    created html

--- src/routes/colors/+page.svelte
+++ src/routes/colors/+page.svelte
@@ -10,9 +10,30 @@
 </script>
 
 <div class="container">
-
+    <div class="color-list">
+        {#each colors as color (color.id)}
+            <div class="color" style={`background-color: ${color.color}`}></div>
+        {/each}
+    </div>
 </div>
 
 <style>
+    .container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        height: 90vh;
+    }
 
+    .color {
+        height: 7rem;
+        width: 6rem;
+    }
+    .color-list {
+        display: flex;
+        gap: 0.2rem;
+        overflow: hidden;
+        width: 100vw;
+    }
 </style>
