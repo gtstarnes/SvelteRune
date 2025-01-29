9e66b531e78d4ee16ad177fde437a96d3270f289
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-01-29 09:23:38 -0500

    reset

--- src/routes/dark/button.svelte
+++ src/routes/dark/button.svelte
@@ -3,30 +3,11 @@
  * REP: III
  */
 
-import Moon from "./moon.svelte";
-import Sun from "./sun.svelte";
 
-let toggle = $state(false)
-
-const toggleDark = () => {
-    toggle = !toggle
-    document.querySelector("body")?.classList.toggle("dark-mode")
-}
 </script>
 
-{#if toggle === true}
-    <button onclick={toggleDark}><Sun /></button>
-{:else}
-    <button onclick={toggleDark}><Moon /></button>
-{/if}
+
 
 <style>
-    button {
-        border: none;
-        background-color: inherit;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        cursor: pointer;
-    }
+
 </style>
