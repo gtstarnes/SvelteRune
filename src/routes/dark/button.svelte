d7bc3adb531927016325f43fe93d1784a9c36682
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-02-03 08:11:22 -0500

    created conditional rendering

--- src/routes/dark/button.svelte
+++ src/routes/dark/button.svelte
@@ -3,11 +3,18 @@
  * REP: IV
  */
 
+	import Moon from "./moon.svelte";
+	import Sun from "./sun.svelte";
+
  const toggle = $state<boolean>(false);
 
 </script>
 
-
+{#if toggle}
+    <button><Moon /></button>
+{:else}
+    <button><Sun /></button>
+{/if}
 
 <style>
 
