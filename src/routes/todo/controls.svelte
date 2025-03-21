90e7ea91a9b3afc76d126675f6584cc4f244c55a
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-03-21 13:59:24 -0400

    create html

--- src/routes/todo/controls.svelte
+++ src/routes/todo/controls.svelte
@@ -7,6 +7,16 @@
 
 </script>
 
+<form>
+    <span>
+        <input type="text" placeholder="add a task..." />
+        <div>
+            <button>Add</button>
+            <button>Remove Completed</button>
+        </div>
+    </span>
+    <span></span>
+</form>
 
 
 <style></style>
