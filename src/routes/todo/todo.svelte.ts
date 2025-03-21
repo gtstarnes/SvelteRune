3faf1167f1b2b545bd316ae0405acef40ab390b7
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-03-21 13:55:39 -0400

    fix addTask

--- src/routes/todo/todo.svelte.ts
+++ src/routes/todo/todo.svelte.ts
@@ -13,11 +13,21 @@ let tasks: Task[] = [];
 function getTasks(){
     return tasks;
 }
-function addTask(task:Task){
-    tasks = [...tasks, task]
+function addTask(task:string){
+    const newTask = {
+        task: task,
+        status: false,
+        edit: false,
+    }
+    tasks = [...tasks, newTask]
 }
 function delTask(task:string){
     tasks = tasks.filter(t => {
         return t.task !== task
+    })
+}
+function massDelete(){
+    tasks = tasks.filter(t => {
+        return t.status !== true;
     })
 }
