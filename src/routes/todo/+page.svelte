452e480d4cacbcfbce35f61c83de5c8db5799645
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-03-21 13:49:38 -0400

    reset

--- src/routes/todo/+page.svelte
+++ src/routes/todo/+page.svelte
@@ -3,57 +3,13 @@
      * REP: 3
      */
 
-	import Controls from "./controls.svelte";
-	import { deleteTask, getTasks, toggleComplete } from "./todo.svelte";
 
-    const tasks = $derived(getTasks())
 </script>
 
 <div>
-    <Controls />
-    <div>
-        {#if tasks.length === 0}
-            There are no tasks
-        {:else}
-            <ul>
-                {#each tasks as task(task)}
-                    <li>
-                        <div role="button" 
-                            tabindex="0"
-                            class="task"
-                            class:done={task.complete === true}
-                            ondblclick={() => toggleComplete(task.text)}>
-                                {task.text}
-                        </div>
-                        <span>
-                            <button>Edit</button>
-                            <button onclick={() => deleteTask(task.text)}>Delete</button>
-                        </span>
-                    </li>
-                {/each}
-            </ul>
-        {/if}
-    </div>
+
 </div>
 
 <style>
-    .done{
-        text-decoration: line-through;
-        color: grey;
-    }
-    li {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        padding-right: 10px;
-        padding-left: 10px;
-        height: 2rem;
-        cursor: pointer;
-    }
-    li:hover{
-        background-color: lightgray;
-    }
-    .task {
-        width: 80%;
-    }
+
 </style>
--- src/routes/todo/controls.svelte
+++ src/routes/todo/controls.svelte
@@ -1,42 +1,12 @@
 <script lang="ts">
-	import { addTask, checkTasks, massDelete } from "./todo.svelte";
+	
 
 /**
  * REP: 3
  */
-    let input = $state('');
-    let error = $state('');
 
-    //FUNCTIONS
-    function handleSubmit(e: SubmitEvent){
-        e.preventDefault
-        checkErrors();
-        if (error === ''){
-            addTask(input)
-        }
-        input = ''
-    }
-    function checkErrors(){
-        switch(true){
-            case input === '':
-                return error = "task cannot be empty"
-            case input.length > 20:
-                return error = "tasks cannot be longer than 20 characters"
-            case checkTasks(input):
-                return error = "task already exists"
-            default:
-                return error = ""
-        }
-    }
 </script>
 
-<form onsubmit={handleSubmit}>
-    <input type="text" placeholder="add task..." bind:value={input} />
-    <span>
-        <button type="submit" >Add</button>
-        <button type="button" onclick={massDelete}>Mass Delete</button>
-    </span>
-</form>
-<div>{error}</div>
+
 
 <style></style>
