208ea31def9abebb76b47ab4d9c886a90e1dfb54
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-03-21 14:42:05 -0400

    add styles

--- src/routes/todo/+page.svelte
+++ src/routes/todo/+page.svelte
@@ -16,13 +16,11 @@
         <ul>
             {#each tasks as task (task.task)}
                 <li>
-                    <div>
                         <input type="checkbox" bind:checked={task.status} />
                         <p>{task.task}</p>
                         <span>
                             <button>Delete</button>
                         </span>
-                    </div>
                 </li>
             {/each}
         </ul>
@@ -30,5 +28,23 @@
 </div>
 
 <style>
+    section {
+        height: 90vh;
+        width: 100%;
 
+        ul {
+            list-style: none;
+            width: 100%;
+            margin: 0;
+            padding: 0;
+
+            li{
+                display: flex;
+                height: 4rem;
+                width: 100%;
+                align-items: center ;
+                justify-content: space-between;
+            }
+        }
+    }
 </style>
