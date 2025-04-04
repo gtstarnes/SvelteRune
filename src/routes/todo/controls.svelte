6266a5f8c340d1ffe9a202c967286a2812b760b9
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-04-04 13:01:38 -0400

    add html

--- src/routes/todo/controls.svelte
+++ src/routes/todo/controls.svelte
@@ -3,7 +3,12 @@
 </script>
 
 <div>
-
+    <input type="text" placeholder="what needs doing" />
+    <div>
+        <button>Add Task</button>
+        <button>Mass Select</button>
+        <button>Delete Completed</button>
+    </div>
 </div>
 
 
