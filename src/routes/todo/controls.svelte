6735bdca21525552ed9844aaf42bdf29bf8bea63
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-04-04 13:06:02 -0400

    bound controls

--- src/routes/todo/controls.svelte
+++ src/routes/todo/controls.svelte
@@ -1,16 +1,17 @@
 <script lang="ts">
-	import { massDelTask, massSetStatus } from "./todo.svelte";
+	import { addTask, massDelTask, massSetStatus } from "./todo.svelte";
 
-
+    let input = $state("");
 </script>
 
 <div>
-    <input type="text" placeholder="what needs doing" />
+    <input type="text" placeholder="what needs doing..." bind:value={input} />
     <div>
-        <button>Add Task</button>
+        <button onclick={() => addTask(input)}>Add Task</button>
         <button onclick={massSetStatus}>Mass Select</button>
         <button onclick={massDelTask}>Delete Completed</button>
     </div>
+    <div></div>
 </div>
 
 
