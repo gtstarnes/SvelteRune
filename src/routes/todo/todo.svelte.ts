26c008726eb6f5cb68c24c0bebe48fdb4d6815c5
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-04-04 12:23:37 -0400

    create addTask

--- src/routes/todo/todo.svelte.ts
+++ src/routes/todo/todo.svelte.ts
@@ -4,8 +4,15 @@ type Task = {
     edit: boolean,
 }
 
-const tasks = $state<Task[]>([])
+let tasks = $state<Task[]>([])
 
 function getTasks(){
     return tasks;
+}
+function addTask(task:string){
+    tasks = [... tasks, {
+        task: task,
+        status: false,
+        edit: false
+    }]
 }
