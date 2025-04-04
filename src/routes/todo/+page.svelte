6f1038b1fe60205f1bd8ad397cfddb4c49b68be2
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-04-04 13:17:57 -0400

    add html

--- src/routes/todo/+page.svelte
+++ src/routes/todo/+page.svelte
@@ -4,15 +4,35 @@
 
 const tabs = ['All', 'Incomplete', 'Complete'];
 const tasks = $derived(getTasks());
+let active = $state('All')
 </script>
 
 <div>
     <Controls />
     <section>
-        {#each tabs as tab (tab)}
-            <button>{tab}</button>
-        {/each}
+        <div>
+            {#each tabs as tab (tab)}
+                <button>{tab}</button>
+            {/each}
+        </div>
+        {#if tasks.length === 0}
+            <div>There are no tasks</div>
+        {:else}
+            <ul>
+                {#each tasks as task (task.task)}
+                    <li>
+                        <input type="checkbox">
+                        <span>{task.task}</span>
+                        <span>
+                            <button>Edit</button>
+                            <button>Delete</button>
+                        </span>
+                    </li>
+                {/each}
+            </ul>
+        {/if}
     </section>
+
 </div>
 
 <style>
