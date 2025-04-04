3ab8a64a84bbd00903bed1791128903e5fe1b78f
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-04-04 15:18:39 -0400

    add styles

--- src/routes/todo/controls.svelte
+++ src/routes/todo/controls.svelte
@@ -12,7 +12,7 @@
 
 <div>
     <input type="text" placeholder="what needs doing..." bind:value={input} />
-    <div>
+    <div class="controls">
         <button onclick={sendInput}>Add Task</button>
         <button onclick={massSetStatus}>Mass Select</button>
         <button onclick={massDelTask}>Delete Completed</button>
@@ -21,4 +21,22 @@
 </div>
 
 
-<style></style>
+<style>
+    input {
+        width: 100%;
+        height: 2rem;
+        text-align: center;
+        margin-bottom: 0.5rem;
+    }
+    .controls {
+        display: flex;
+        align-content: center;
+        justify-content: space-between;
+
+        button {
+            width: 33%;
+            height: 2rem;
+            cursor: pointer;
+        }
+    }
+</style>
