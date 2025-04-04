a2db1a4cd1d39543bbd009a5c281e3159379b1fd
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-04-04 12:59:08 -0400

    reset

--- src/routes/todo/+page.svelte
+++ src/routes/todo/+page.svelte
@@ -12,28 +12,5 @@
 </div>
 
 <style>
-    section {
-        height: 90vh;
-        width: 100%;
 
-        ul {
-            list-style: none;
-            width: 100%;
-            margin: 0;
-            padding: 0;
-
-            li{
-                display: flex;
-                height: 4rem;
-                width: 100%;
-                align-items: center ;
-                justify-content: space-between;
-
-                .active {
-                    text-decoration: line-through;
-                    color: grey
-                }
-            }
-        }
-    }
 </style>
