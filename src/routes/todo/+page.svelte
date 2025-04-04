7147a65a6c411c5df8cf281e280ec76450ad0cdd
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-04-04 13:11:21 -0400

    create variables

--- src/routes/todo/+page.svelte
+++ src/routes/todo/+page.svelte
@@ -1,11 +1,19 @@
 <script lang="ts">
 	import Controls from "./controls.svelte";
+	import { getTasks } from "./todo.svelte";
 
-
+const tabs = ['All', 'Incomplete', 'Complete'];
+const tasks = $derived(getTasks());
 </script>
 
 <div>
     <Controls />
+    <div>
+        
+    </div>
+    <section>
+
+    </section>
 </div>
 
 <style>
